09e9877176a656175199eb661192080adeb10875
Alexey Gordeev <goa@arenadata.io>
2021-09-28 15:33:37 +0500

    A better version of possible fix №1

--- src/backend/commands/indexcmds.c
+++ src/backend/commands/indexcmds.c
@@ -838,11 +838,11 @@ DefineIndex(Oid relationId,
 					 stmt->concurrent, !check_rights,
 					 &createdConstraintId);
 
-	PopActiveSnapshot();
-	PushActiveSnapshot(GetTransactionSnapshot());
-
 	if (shouldDispatch)
 	{
+		PopActiveSnapshot();
+		PushActiveSnapshot(GetTransactionSnapshot());
+		
 		/* make sure the QE uses the same index name that we chose */
 		stmt->idxname = indexRelationName;
 		stmt->oldNode = InvalidOid;
