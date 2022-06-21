20dcb1efd86cb826092a2069c84f84a45d0a2a6f
Alexey Gordeev <goa@arenadata.io>
2022-06-21 20:48:11 +0500

    Fix cross-slice interaction detection for subplans
    GPDB has Shared Scan node to share the output of a subplan. Each Shared Scan
    node can act like producer or consumer. Each one may be a part of different
    slice. One of essential parts of Shared Scan processing is cross-slice
    interaction detection. It helps to find the difference in producer's and
    consumer's slices and mark underlying Material or Sort nodes as cross-slice.
    Cross-slice producer reads all the tuples from child plan before the consumer
    Shared Scan can read it.
    
    Before this patch, cross-slice interaction detection of underlying subplan nodes
    was broken. We walked through subplans separately from main plan. This, in case
    subplan has no motion, caused `shareinput_mutator_xslice_2()` to think we don't
    need to mark node as cross-slice - motion stack (`motStack`) used for
    cross-slice interaction detection contained only default motion id (0) which
    equals to producer's slice id.
    
    Here is the simplified part of new regression test. It shows Shared Scan
    consumer under Subplan, which has different (compared to producer upper) slice
    id.
    ```
     Sequence
       ->  Shared Scan (share slice:id 0:0)
         ->  Seq Scan on t1 t1_1
       ->  Gather Motion 3:1  (slice3; segments: 3)
         ->  Redistribute Motion 1:3  (slice1)
           ->  Function Scan on generate_series
             SubPlan 1  (slice1)
               ->  Shared Scan (share slice:id 1:0)
    ```
    Before this patch, we didn't have upper motions in `motStack`. From now, we dive
    into subplans as parts of main plan according to the plan tree hierarchy.
    Cross-slice detector understands that our slice(1) is different from producer's
    slice(0) and marks node as cross-slice.
    
    Existing logic and comments around rtables left almost unchanged.
    Existing tricky logic around joins execution order changed to another tricky
    logic which is compatible with `plan_tree_walker()`.
    `gporca_optimizer.out` test output changed as we now start
    `shareinput_mutator_dag_to_tree()` from the main plan. Before, we started from
    subplans and global share input context contained only 3 producers at the time
    `set_plan_share_id()` called. Now, at the same place producers count is equal to
    4, so RangeTblEntry's name changed.

--- src/backend/executor/execMain.c
+++ src/backend/executor/execMain.c
@@ -3028,6 +3028,7 @@ ExecutePlan(EState *estate,
 {
 	TupleTableSlot *slot;
 	long		current_tuple_count;
+	ListCell *lc;
 
 	/*
 	 * For holdable cursor, the plan is executed without rewinding on gpdb. We
@@ -3049,6 +3050,12 @@ ExecutePlan(EState *estate,
 	/*
 	 * Make sure slice dependencies are met
 	 */
+	foreach(lc, estate->es_subplanstates)
+	{
+		PlanState	   *splanstate = (PlanState *) lfirst(lc);
+
+		ExecSliceDependencyNode(splanstate);
+	}
 	ExecSliceDependencyNode(planstate);
 
 #ifdef FAULT_INJECTOR
