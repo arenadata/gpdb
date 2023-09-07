8317d920cde7c25c4366f6110f3c21dd1476b867
Evgeniy Ratkov <e.ratkov@arenadata.io>
2023-09-07 11:33:07 +0300

    arenadata_toolkit: add support for working with partitions to adb_skew_coefficients (#607)
    Before this patch adb_skew_coefficients worked only with the last level of partitions correctly.
    For other parent partitions and root of partitions adb_skew_coefficients can not show coeffcients
    because select sizes from adb_relation_storage_size returns sizes only for last level of partitions.
    
    View adb_skew_coefficients was rewrited to work with new function get_full_storage_size.
    Function get_full_storage_size calculates sizes for all levels of partitions and returns table with sizes.

--- gpcontrib/arenadata_toolkit/arenadata_toolkit--1.0--1.1.sql
+++ gpcontrib/arenadata_toolkit/arenadata_toolkit--1.0--1.1.sql
@@ -1,3 +1,41 @@
 /* gpcontrib/arenadata_toolkit/arenadata_toolkit--1.0--1.1.sql */
 
 ALTER FUNCTION arenadata_toolkit.adb_get_relfilenodes(tablespace_oid OID) ROWS 30000000;
+
+CREATE OR REPLACE VIEW arenadata_toolkit.adb_skew_coefficients
+AS
+WITH recursive cte AS (
+	WITH all_tables_sizes AS (
+		SELECT t1.autrelname relname, t1.autoid id, inhparent parent_id,
+				arenadata_toolkit.adb_relation_storage_size(t1.autoid) AS size
+			FROM gp_dist_random('gp_toolkit.__gp_user_tables') t1
+			LEFT JOIN pg_inherits t2 ON inhrelid = t1.autoid
+			WHERE autrelstorage != 'x')
+		SELECT *, ROW_NUMBER () OVER (PARTITION BY id
+										ORDER BY id) AS seg_id
+			FROM all_tables_sizes
+	UNION ALL
+		SELECT t1.autrelname relname, t1.autoid id, inhparent parent_id, size, seg_id
+			FROM gp_toolkit.__gp_user_tables t1
+			LEFT JOIN pg_inherits t2 ON inhrelid = t1.autoid
+			JOIN cte ON cte.parent_id = t1.autoid
+			WHERE autrelstorage != 'x'
+	),
+	tables_size_by_segments AS (
+		SELECT id, sum(size) as size, seg_id
+			FROM cte
+			GROUP BY (id, seg_id)),
+	skew AS (
+		SELECT id AS skewoid, stddev(size) AS skewdev, avg(size) AS skewmean
+			FROM tables_size_by_segments
+			GROUP BY id)
+	SELECT
+		skew.skewoid AS skcoid,
+		pgn.nspname  AS skcnamespace,
+		pgc.relname  AS skcrelname,
+		CASE WHEN skewdev > 0 THEN skewdev/skewmean * 100.0 ELSE 0 END AS skccoeff
+	FROM skew
+	JOIN pg_catalog.pg_class pgc ON (skew.skewoid = pgc.oid)
+	JOIN pg_catalog.pg_namespace pgn ON (pgc.relnamespace = pgn.oid);
+
+GRANT SELECT ON arenadata_toolkit.adb_skew_coefficients TO public;
