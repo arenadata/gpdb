edec28e6569b71fa85a1f513917432d55b3f2a8a
Viktor Kurilko <v.kurilko@arenadata.io>
2023-10-10 11:13:08 +0700

    change comment

--- src/backend/cdb/cdbmutate.c
+++ src/backend/cdb/cdbmutate.c
@@ -2089,7 +2089,8 @@ shareinput_mutator_dag_to_tree(Node *node, PlannerInfo *root, bool fPop)
 	{
 		/*
 		 * HashJoin requires prefetch_inner to be enabled if there is a
-		 * SharedScan in the plan even if it is on bottleneck.
+		 * SharedScan in the plan but in the case of bottleneck, it can be
+		 * turned off for optimization reasons, so we turn it on again.
 		 */
 		if (IsA(plan, HashJoin) && ctxt->producer_count > 0 &&
 			CdbPathLocus_IsBottleneck(*(plan->flow)))
