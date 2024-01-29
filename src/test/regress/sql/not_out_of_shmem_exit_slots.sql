6b72b981ee0a740bf756cb52f687a69bcbdd0216
Evgeniy Ratkov <e.ratkov@arenadata.io>
2024-01-29 10:38:42 +0300

    query optimization

--- src/test/regress/sql/not_out_of_shmem_exit_slots.sql
+++ src/test/regress/sql/not_out_of_shmem_exit_slots.sql
@@ -189,17 +189,16 @@ BEGIN
 	FOR nsp IN
 		SELECT distinct nspname
 		FROM (
-			SELECT nspname,
-			       regexp_replace(nspname, 'pg(_toast)?_temp_', '')::int as sess_id
+			SELECT nspname
 			FROM gp_dist_random('pg_namespace')
 			WHERE nspname ~ '^pg(_toast)?_temp_[0-9]+'
 			UNION
-			SELECT nspname,
-			       regexp_replace(nspname, 'pg(_toast)?_temp_', '')::int as sess_id
+			SELECT nspname
 			FROM pg_namespace
 			WHERE nspname ~ '^pg(_toast)?_temp_[0-9]+'
 		) n
-		LEFT OUTER JOIN pg_stat_activity x using (sess_id)
+		LEFT OUTER JOIN pg_stat_activity x on
+			sess_id = regexp_replace(nspname, 'pg(_toast)?_temp_', '')::int
 		WHERE x.sess_id is null
 	LOOP
 		EXECUTE format('DROP SCHEMA IF EXISTS %I CASCADE', nsp);
