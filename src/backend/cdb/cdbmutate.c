a99c4b16fccea38460afde7dc28d94641d87afb1
Vladimir Sarmin <v.sarmin@arenadata.io>
2024-01-30 14:43:51 +0300

    Fix nMotionsAbove not decrementing when we already found motions

--- src/backend/cdb/cdbmutate.c
+++ src/backend/cdb/cdbmutate.c
@@ -71,9 +71,10 @@ typedef struct ModifyTableMotionState
 	bool		needExplicitMotion;
 	int			nMotionsAbove;			/* Number of Redistribute/Broadcast
 										 * motions above the current node */
-	bool		isChecking;				/* True if we encountered ModifyTable
-										 * node with UPDATE/DELETE and we plan
-										 * to insert Explicit Motions */
+	bool		isChecking;				/* True if we are currently checking
+										 * the plan for motions */
+	bool 		isEnabled;				/* True if we encountered ModifyTable
+										 * node with UPDATE/DELETE */
 } ModifyTableMotionState;
 
 /*
@@ -422,6 +423,7 @@ apply_motion(PlannerInfo *root, Plan *plan, Query *query)
 	state.mt.resultReloids = NULL;
 	state.mt.needExplicitMotion = false;
 	state.mt.nMotionsAbove = 0;
+	state.mt.isEnabled = false;
 	state.mt.isChecking = false;
 
 	memset(&ctl, 0, sizeof(ctl));
@@ -828,6 +830,7 @@ apply_motion_mutator(Node *node, ApplyMotionState *context)
 			 */
 			Assert(context->mt.resultReloids == NULL);
 			Assert(context->mt.nMotionsAbove == 0);
+			Assert(!context->mt.isEnabled);
 			Assert(!context->mt.needExplicitMotion);
 
 			/*
@@ -847,6 +850,7 @@ apply_motion_mutator(Node *node, ApplyMotionState *context)
 														   relid);
 			}
 
+			context->mt.isEnabled = true;
 			context->mt.isChecking = true;
 		}
 	}
@@ -1148,7 +1152,7 @@ done:
 	plan->nMotionNodes = context->nextMotionID - saveNextMotionID;
 	plan->nInitPlans = hash_get_num_entries(context->planid_subplans) - saveNumInitPlans;
 
-	if (context->mt.isChecking && IsA(node, Motion))
+	if (context->mt.isEnabled && IsA(node, Motion))
 	{
 		Motion	   *motion = (Motion *) node;
 
