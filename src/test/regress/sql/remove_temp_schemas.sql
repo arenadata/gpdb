850e185428d059912bd4bdcd99bd48fc888a0476
Evgeniy Ratkov <e.ratkov@arenadata.io>
2024-02-07 13:56:54 +0300

    remove unnecessary short table name

--- src/test/regress/sql/remove_temp_schemas.sql
+++ src/test/regress/sql/remove_temp_schemas.sql
@@ -11,9 +11,9 @@ BEGIN
 			SELECT nspname
 			FROM pg_namespace
 		) n
-		LEFT JOIN pg_stat_activity x ON
+		LEFT JOIN pg_stat_activity ON
 			sess_id = regexp_replace(nspname, 'pg(_toast)?_temp_', '')::int
-		WHERE x.sess_id is null AND nspname ~ '^pg(_toast)?_temp_[0-9]+'
+		WHERE sess_id is null AND nspname ~ '^pg(_toast)?_temp_[0-9]+'
 	LOOP
 		EXECUTE format('DROP SCHEMA IF EXISTS %I CASCADE', nsp);
 	END LOOP;
