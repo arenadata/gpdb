117ddffb681f999c6a4c4200f661dc62ebd4fad3
Vladimir Sarmin <v.sarmin@arenadata.io>
2024-02-09 11:10:56 +0300

    Revert "Add a null check for port->hba"
    This reverts commit e119ffbdf7afc92379d163aca98a345924620155.

--- src/backend/libpq/auth.c
+++ src/backend/libpq/auth.c
@@ -282,7 +282,6 @@ auth_failed(Port *port, int status, char *logdetail)
 	{
 		errstr = gettext_noop("authentication failed for user \"%s\": "
 							  "invalid authentication method");
-		cdetail = NULL;
 	}
 	else
 	{
@@ -330,15 +329,16 @@ auth_failed(Port *port, int status, char *logdetail)
 			errstr = gettext_noop("authentication failed for user \"%s\": invalid authentication method");
 			break;
 	  }
-	  cdetail = psprintf(_("Connection matched pg_hba.conf line %d: \"%s\""),
-						   port->hba->linenumber, port->hba->rawline);
 	}
+
+	cdetail = psprintf(_("Connection matched pg_hba.conf line %d: \"%s\""),
+					   port->hba->linenumber, port->hba->rawline);
 
     /*
      * Avoid leak user infomations when failed to connect database using LDAP,
      * and we need hide failed details return by LDAP.
      * */
-    if (port->hba && port->hba->auth_method == uaLDAP)
+    if (port->hba->auth_method == uaLDAP)
     {
         pfree(cdetail);
         cdetail = NULL;
