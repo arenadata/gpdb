ee1a8d8f77b64edb54638bbf023633bb56b1db33
Georgy Shelkovy <g.shelkovy@arenadata.io>
2024-02-20 10:57:04 +0500

    reorder

--- src/backend/gporca/libnaucrates/src/parser/CParseHandlerRedistributeMotion.cpp
+++ src/backend/gporca/libnaucrates/src/parser/CParseHandlerRedistributeMotion.cpp
@@ -168,21 +168,22 @@ CParseHandlerRedistributeMotion::EndElement(
 	CParseHandlerPhysicalOp *child_parse_handler =
 		dynamic_cast<CParseHandlerPhysicalOp *>((*this)[5]);
 
+	GPOS_ASSERT(NULL != prop_parse_handler);
+	GPOS_ASSERT(NULL != proj_list_parse_handler);
+	GPOS_ASSERT(NULL != filter_parse_handler);
+	GPOS_ASSERT(NULL != sort_col_list_parse_handler);
+	GPOS_ASSERT(NULL != hash_expr_list_parse_handler);
+	GPOS_ASSERT(NULL != child_parse_handler);
+
 	m_dxl_node = GPOS_NEW(m_mp) CDXLNode(m_mp, m_dxl_op);
 	// set statictics and physical properties
-	GPOS_ASSERT(NULL != prop_parse_handler);
 	CParseHandlerUtils::SetProperties(m_dxl_node, prop_parse_handler);
 
 	// add children
-	GPOS_ASSERT(NULL != proj_list_parse_handler);
 	AddChildFromParseHandler(proj_list_parse_handler);
-	GPOS_ASSERT(NULL != filter_parse_handler);
 	AddChildFromParseHandler(filter_parse_handler);
-	GPOS_ASSERT(NULL != sort_col_list_parse_handler);
 	AddChildFromParseHandler(sort_col_list_parse_handler);
-	GPOS_ASSERT(NULL != hash_expr_list_parse_handler);
 	AddChildFromParseHandler(hash_expr_list_parse_handler);
-	GPOS_ASSERT(NULL != child_parse_handler);
 	AddChildFromParseHandler(child_parse_handler);
 
 	// deactivate handler
