8346734f78d5891b93561b7cfba58c30e6351773
Georgy Shelkovy <g.shelkovy@arenadata.io>
2024-02-20 12:24:17 +0500

    reorder

--- src/backend/gporca/libnaucrates/src/parser/CParseHandlerScalarSubPlanParamList.cpp
+++ src/backend/gporca/libnaucrates/src/parser/CParseHandlerScalarSubPlanParamList.cpp
@@ -137,8 +137,8 @@ CParseHandlerScalarSubPlanParamList::EndElement(
 	{
 		CParseHandlerScalarSubPlanParam *parse_handler_subplan_param =
 			dynamic_cast<CParseHandlerScalarSubPlanParam *>((*this)[ul]);
-
 		GPOS_ASSERT(NULL != parse_handler_subplan_param);
+
 		CDXLColRef *dxl_colref = parse_handler_subplan_param->MakeDXLColRef();
 		dxl_colref->AddRef();
 		m_dxl_colref_array->Append(dxl_colref);
