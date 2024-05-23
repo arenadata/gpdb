102c1d9d67b742983bdd2f81acd9b85ed6ee7db1
Vladimir Sarmin <v.sarmin@arenadata.io>
2024-05-23 09:02:58 +0300

    Avoid recreating the table in tests

--- src/test/regress/sql/with.sql
+++ src/test/regress/sql/with.sql
@@ -1186,12 +1186,9 @@ WITH with_test AS (SELECT 42) INSERT INTO with_test VALUES (1);
 create temp table with_test (i int);
 with with_test as (select 42) insert into with_test select * from with_test;
 select * from with_test;
-drop table with_test;
 
 -- Make sure we create a correct tuple descriptor for SubqueryScan above
 -- ShareInputScan with ModifyTable below.
-CREATE TABLE with_test (i int) DISTRIBUTED BY (i);
-
 EXPLAIN (VERBOSE, COSTS OFF) WITH cte AS (
 	INSERT INTO with_test SELECT 1 RETURNING *
 )
