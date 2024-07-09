9b78965bae5ec983f17a97f0f10a1e23f75666e0
Maksim Michkov <m.michkov@arenadata.io>
2024-07-09 16:12:11 +0000

    Fix tests

--- src/test/isolation2/sql/copy_interrupt.sql
+++ src/test/isolation2/sql/copy_interrupt.sql
@@ -1,13 +1,14 @@
 !\retcode gpconfig -c client_connection_check_interval -v 20s;
 !\retcode gpstop -u;
 
-0: CREATE TABLE t(a int);
-0&: COPY t FROM PROGRAM 'while true; do echo 1; sleep 1; done | cat -';
+0: CREATE TABLE copy_interrupt_table(a int);
+0&: COPY copy_interrupt_table FROM PROGRAM 'while true; do echo 1; sleep 1; done | cat -';
+! sleep 10;
 
 0t:
 ! sleep 40;
 
 SELECT pg_terminate_backend(pid)
-    FROM pg_stat_activity where query LIKE 'COPY t FROM PROGRAM%';
+    FROM pg_stat_activity where query LIKE 'COPY copy_interrupt_table FROM PROGRAM%';
 
-DROP TABLE t;
+DROP TABLE copy_interrupt_table;
