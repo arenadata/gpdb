22b8407a0776c0712c414617fcfc761cb1540cef
Andrey Sokolov <a.sokolov@arenadata.io>
2024-08-01 08:27:26 +0300

    Add dockerfile which is based on Ubuntu 22.04
    There is no sudo in the ubuntu:22.04 image, so sudo is moved from
    README.ubuntu.bash to README.linux.md. Add packages, which are required to build
    GPDB, to README.ubuntu.bash. The new Dockerfile.ubuntu file is similar to
    arenadata/Dockerfile. The first RUN setups the environment to run
    compile_gpdb.bash, the last - to run regression tests.

--- README.ubuntu.bash
+++ README.ubuntu.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -y \
+apt-get update
+apt-get install -y \
   bison \
   cmake \
   curl \
@@ -11,20 +11,26 @@ sudo apt-get install -y \
   git \
   iputils-ping \
   libapr1-dev \
+  libaprutil1-dev \
   libbz2-dev \
   libcurl4-openssl-dev \
   libevent-dev \
   libkrb5-dev \
+  libpam-dev \
   libperl-dev \
   libreadline-dev \
   libssl-dev \
+  libtool \
+  libuv1-dev \
   libxml2-dev \
+  libxslt-dev \
   libyaml-dev \
   libzstd-dev \
   locales \
   net-tools \
   openssh-client \
   openssh-server \
+  pkg-config \
   python2 \
   python2-dev \
   zlib1g-dev
