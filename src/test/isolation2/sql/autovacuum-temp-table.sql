bdca70d34bde42fa536c96a22e5b8aaa2137eb5c
Maksim Michkov <m.michkov@arenadata.io>
2024-08-07 11:37:29 +0000

    Autovacuum info (good)

--- src/test/isolation2/sql/autovacuum-temp-table.sql
+++ src/test/isolation2/sql/autovacuum-temp-table.sql
@@ -1,9 +1,22 @@
+1: CREATE OR REPLACE FUNCTION show_on_segments(show_name text) RETURNS table (guc_name text, guc_setting text) EXECUTE ON ALL SEGMENTS AS $$ BEGIN	RETURN QUERY SELECT name, setting FROM pg_settings WHERE show_name = name; END; $$ LANGUAGE plpgsql;
+1: SHOW autovacuum_naptime;
+1: SELECT * FROM show_on_segments('autovacuum_naptime');
+1: SHOW autovacuum_vacuum_threshold;
+1: SELECT * FROM show_on_segments('autovacuum_vacuum_threshold');
 -- test that autovacuum cleans up orphaned temp table correctly
 
 -- speed up test
 1: alter system set autovacuum_naptime = 5;
 1: alter system set autovacuum_vacuum_threshold = 50;
 1: !\retcode gpstop -u;
+
+1: SHOW autovacuum_naptime;
+1: SELECT * FROM show_on_segments('autovacuum_naptime');
+1: SHOW autovacuum_vacuum_threshold;
+1: SELECT * FROM show_on_segments('autovacuum_vacuum_threshold');
+
+1: SELECT * FROM pg_namespace where nspname like 'pg_temp_%' and oid > 16386;
+1: SELECT n.nspname, c.relname FROM pg_class c JOIN pg_namespace n ON c.relnamespace = n.oid WHERE n.nspname like 'pg_temp_%' AND n.oid > 16386;
 
 -- session 1 is going to panic on primary segment 0, creating orphaned temp table on all QEs;
 -- session 2 will remain, so the temp table on non-PANIC segment will remain too (until the session resets or exits)
@@ -14,6 +27,9 @@
 0U: create temp table ttu_av0(a int);
 1U: create temp table ttu_av1(a int);
 
+1: SELECT * FROM pg_namespace where nspname like 'pg_temp_%' and oid > 16386;
+1: SELECT n.nspname, c.relname FROM pg_class c JOIN pg_namespace n ON c.relnamespace = n.oid WHERE n.nspname like 'pg_temp_%' AND n.oid > 16386;
+
 -- Inject a PANIC on one of the segment. Use something that's not going to be hit by background worker (including autovacuum).
 1: select gp_inject_fault('create_function_fail', 'panic', dbid) from gp_segment_configuration where content=0 and role='p';
 1: create function my_function() returns void as $$ begin end; $$ language plpgsql;
@@ -21,12 +37,23 @@
 0Uq:
 1q:
 
+1: SELECT * FROM pg_namespace where nspname like 'pg_temp_%' and oid > 16386;
+1: SELECT n.nspname, c.relname FROM pg_class c JOIN pg_namespace n ON c.relnamespace = n.oid WHERE n.nspname like 'pg_temp_%' AND n.oid > 16386;
+
 -- make sure the segment restarted,
 -- also served as a third test case.
 1: create temp table tt_av3(a int);
 
+1: SELECT * FROM pg_namespace where nspname like 'pg_temp_%' and oid > 16386;
+1: SELECT n.nspname, c.relname FROM pg_class c JOIN pg_namespace n ON c.relnamespace = n.oid WHERE n.nspname like 'pg_temp_%' AND n.oid > 16386;
+
 -- clear the fault
 1: select gp_inject_fault('create_function_fail', 'reset', dbid) from gp_segment_configuration where content=0 and role='p';
+
+1: SHOW autovacuum_naptime;
+1: SELECT * FROM show_on_segments('autovacuum_naptime');
+1: SHOW autovacuum_vacuum_threshold;
+1: SELECT * FROM show_on_segments('autovacuum_vacuum_threshold');
 
 -- make sure the autovacuum is run at least once,
 -- it might've been run already, which is fine.
@@ -38,6 +65,9 @@
 1: select gp_wait_until_triggered_fault('auto_vac_worker_after_report_activity', 1, dbid) from gp_segment_configuration where content=0 and role='p';
 -- clean up fault
 1: select gp_inject_fault('auto_vac_worker_after_report_activity', 'reset', dbid) from gp_segment_configuration where content=0 and role='p';
+
+1: SELECT * FROM pg_namespace where nspname like 'pg_temp_%' and oid > 16386;
+1: SELECT n.nspname, c.relname FROM pg_class c JOIN pg_namespace n ON c.relnamespace = n.oid WHERE n.nspname like 'pg_temp_%' AND n.oid > 16386;
 
 -- the orphaned temp table on all QEs should be cleaned up
 0U: select count(*) from pg_class where relname = 'tt_av1';
@@ -61,3 +91,23 @@
 1: alter system reset autovacuum_naptime;
 1: alter system reset autovacuum_vacuum_threshold;
 1: !\retcode gpstop -u;
+
+1: SHOW autovacuum_naptime;
+1: SELECT * FROM show_on_segments('autovacuum_naptime');
+1: SHOW autovacuum_vacuum_threshold;
+1: SELECT * FROM show_on_segments('autovacuum_vacuum_threshold');
+
+1: SELECT * FROM pg_namespace where nspname like 'pg_temp_%' and oid > 16386;
+1: SELECT n.nspname, c.relname FROM pg_class c JOIN pg_namespace n ON c.relnamespace = n.oid WHERE n.nspname like 'pg_temp_%' AND n.oid > 16386;
+
+1q:
+2q:
+0Uq:
+1Uq:
+
+1000: SHOW autovacuum_naptime;
+1000: SELECT * FROM show_on_segments('autovacuum_naptime');
+1000: SHOW autovacuum_vacuum_threshold;
+1000: SELECT * FROM show_on_segments('autovacuum_vacuum_threshold');
+1000: SELECT * FROM pg_namespace where nspname like 'pg_temp_%' and oid > 16386;
+1000: SELECT n.nspname, c.relname FROM pg_class c JOIN pg_namespace n ON c.relnamespace = n.oid WHERE n.nspname like 'pg_temp_%' AND n.oid > 16386;
