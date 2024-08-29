ba2d9fd3f1220486f8687ebefd116d99a7ba5181
Maxim Michkov <m.michkov@arenadata.io>
2024-08-29 08:04:54 +0300

    Fix temporary schema cleanup after segment failure (#1022)
    After some segment failures (such as during long insert operations) temporary
    schema wasn't deleted even after the session exits. This error was caused by a
    complicated interaction between two places that delete temporary tables:
    RemoveTempRelationsCallback and GpDropTempTables.
    
    This patch refactors these two functions to make their actions more clear.
    GpDropTempTables is split into two functions: GpResetSessionIfNeeded and
    GpDropTempTables, and RemoveTempRelationsCallaback reuses GpDropTempTables.
    There is also a new function GpHasTempRelationsForDeletion that is used to
    check if any cleanup is necessary. Cancelling the RemoveTempRelationsCallback
    is also moved from ResetTempNamespace (which doesn't fully delete it yet, so
    the callback might still be needed) to GpDropTempTables. With this the issue
    is now fixed.
    
    Changes from original commit:
    1. Function equivalent to GpDropTempTables in GPDB 7 was named
    CheckForResetSession in GPDB 6. We keep it for backwards compatibility,
    now calling GpResetSessionIfNeeded and GpDropTempTables in it
    (same as in GPDB 7).
    2. ResetAllGangs is missing in GPDB 6, no fix needed.
    3. postgres.c already uses CheckForResetSession in GPDB 6, no fix needed.
    4. Fix autovacuum issue by using gpcheckcat instead.
    5. Add a delay before checking whether the table was dropped.
    
    (cherry picked from commit 465f4699b84930f439b75d1118657530e38414a1)

--- src/test/isolation2/sql/orphan_temp_table.sql
+++ src/test/isolation2/sql/orphan_temp_table.sql
@@ -16,7 +16,7 @@
 1: SELECT count(*) FROM pg_namespace where (nspname like '%pg_temp_%' or nspname like '%pg_toast_temp_%') and oid > 16386;
 
 
--- the temp table is left on segment 0, it should be dropped by autovacuum later
+-- the temp table is left on segment 0, it should be dropped by gpcheckcat later
 0U: SELECT relname FROM pg_class where relname = 'test_temp_table_cleanup';
 
 -- no temp table left on other segments
@@ -25,11 +25,47 @@
 1: SELECT gp_inject_fault('before_exec_scan', 'reset', dbid) FROM gp_segment_configuration WHERE role='p' AND content = 0;
 1q:
 
+!\retcode gpcheckcat -O -R orphaned_toast_tables isolation2test;
+
+-- no temp table left
+0U: SELECT relname FROM pg_class where relname = 'test_temp_table_cleanup';
+
 -- case 2: Test if temp table will be left on the coordinator, when session exits in coordinator within a transaction block.
 2: CREATE TEMP TABLE test_temp_table_cleanup(a int);
 2: begin;
 2: select * from test_temp_table_cleanup;
 2q:
 
+-- wait until cleanup is complete
+-- FIXME: there should be a better way to do this
+!\retcode sleep 3;
+
 3: select count(*) from pg_class where relname = 'test_temp_table_cleanup';
 3q:
+
+-- case 3: Test if temp namespace will be left if session exits during a long insert operation
+
+4: CREATE TEMP TABLE test_temp_table_cleanup(a int);
+4: BEGIN;
+-- simulate a long insert query
+4: SELECT gp_inject_fault('heap_insert', 'infinite_loop', '', '',
+   'test_temp_table_cleanup', 1, 1, 0, dbid) FROM gp_segment_configuration
+   WHERE content = 0 AND role = 'p';
+4&: INSERT INTO test_temp_table_cleanup SELECT generate_series(1, 100);
+
+-- trigger a panic on the segment
+5: SELECT gp_inject_fault('create_function_fail', 'panic', dbid) FROM gp_segment_configuration WHERE role='p' AND content = 0;
+5: CREATE FUNCTION my_function() RETURNS void AS $$ BEGIN END; $$ LANGUAGE plpgsql;
+
+-- the insert query should have failed
+4<:
+4q:
+
+5: CREATE TABLE ensure_segment_is_up(a int);
+5: DROP TABLE ensure_segment_is_up;
+
+5: SELECT gp_inject_fault('create_function_fail', 'reset', dbid) FROM gp_segment_configuration WHERE role='p' AND content = 0;
+5: SELECT gp_inject_fault('heap_insert', 'reset', dbid) FROM gp_segment_configuration WHERE role='p' AND content = 0;
+-- there shouldn't be any temporary namespaces left after the session quits
+5: SELECT count(*) FROM pg_namespace where (nspname like '%pg_temp_%' or nspname like '%pg_toast_temp_%') and oid > 16386;
+5q:
