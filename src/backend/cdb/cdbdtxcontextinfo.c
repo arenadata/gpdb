GreengageDB/greengage

Fix gpstop -u not updating GUCs on segments (#881)
Backend process GUCs marked as GUC_GPDB_NEED_SYNC are overwritten by coordinator
upon receiving a new connection. The coordinator sends it's configuration to the
segments the same way as if it were a frontend communicating with the
coordinator. This means the default priority for these GUCs becomes client
priority.

Previously, when gpstop -u was executed, it sent a SIGHUP signal, prompting the
coordinator and segments to re-read their configuration files. However, the
priority of options derived from the configuration file is lower than that of
client options. This difference caused the segments to ignore overwritten
GUCs when they re-read their configuration files, and these GUCs were not
re-dispatched by the QD either. This situation resulted in a desynchronization
of GUCs that are required to be synchronized between the coordinator and the
segments.

The solution is to explicitly dispatch the changed configuration from the
coordinator to the segments, similar to how it is done when initializing the
backend process.

When the QD receives a SIGHUP signal, it reads its configuration file to
identify any GUCs that have been modified. If these GUCs are marked with the
GUC_GPDB_NEED_SYNC flag, the QD dispatches SET commands containing these
modified GUCs to the QEs. To differentiate between this type of synchronization
and user-initiated SET commands, a flag GP_OPT_SYNCHRONIZATION_SET has been
introduced to the distributed transaction options.

src/backend/cdb/cdbdtxcontextinfo.c
@@ -120,12 +120,13 @@ DtxContextInfo_CreateOnMaster(DtxContextInfo *dtxContextInfo, bool inCursor,
 			 dtxContextInfo->curcid);
 
 		elog(LOG,
-			 "DtxContextInfo_CreateOnMaster txnOptions = 0x%x, needDtx = %s, explicitBegin = %s, isoLevel = %s, readOnly = %s.",
+			 "DtxContextInfo_CreateOnMaster txnOptions = 0x%x, needDtx = %s, explicitBegin = %s, isoLevel = %s, readOnly = %s, synchronizationSet = %s.",
 			 txnOptions,
 			 (isMppTxOptions_NeedDtx(txnOptions) ? "true" : "false"),
 			 (isMppTxOptions_ExplicitBegin(txnOptions) ? "true" : "false"),
 			 IsoLevelAsUpperString(mppTxOptions_IsoLevel(txnOptions)),
-			 (isMppTxOptions_ReadOnly(txnOptions) ? "true" : "false"));
+			 (isMppTxOptions_ReadOnly(txnOptions) ? "true" : "false"),
+			 (isMppTxOptions_SynchronizationSet(txnOptions) ? "true" : "false"));
 	}
 }
 

arenadata/gpdb

Changes from original commit:
- In `guc-file.l` move the return value of `ProcessConfigFileInternal` to an
out parameter `changed_gucs_out`.
- Add memory context switching to it so that the `changed_gucs_out` list is
in the correct memory context.
- Change GUC parameter in the test from `backslash_quote` to `constraint_exclusion`
since `backslash_quote` now has a sync flag.
(cherry picked from commit 6934f3deb6917ab6128fd327261c1216847fbb77)

src/backend/cdb/cdbdtxcontextinfo.c
@@ -112,12 +112,13 @@ DtxContextInfo_CreateOnCoordinator(DtxContextInfo *dtxContextInfo, bool inCursor
 			 dtxContextInfo->curcid);
 
 		elog((Debug_print_full_dtm ? LOG : DEBUG5),
-			 "DtxContextInfo_CreateOnCoordinator txnOptions = 0x%x, needDtx = %s, explicitBegin = %s, isoLevel = %s, readOnly = %s.",
+			 "DtxContextInfo_CreateOnCoordinator txnOptions = 0x%x, needDtx = %s, explicitBegin = %s, isoLevel = %s, readOnly = %s, synchronizationSet = %s.",
 			 txnOptions,
 			 (isMppTxOptions_NeedDtx(txnOptions) ? "true" : "false"),
 			 (isMppTxOptions_ExplicitBegin(txnOptions) ? "true" : "false"),
 			 IsoLevelAsUpperString(mppTxOptions_IsoLevel(txnOptions)),
-			 (isMppTxOptions_ReadOnly(txnOptions) ? "true" : "false"));
+			 (isMppTxOptions_ReadOnly(txnOptions) ? "true" : "false"),
+			 (isMppTxOptions_SynchronizationSet(txnOptions) ? "true" : "false"));
 	}
 }
 