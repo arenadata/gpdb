5d6af43ab91ea7ffbb9f6cf76dbcabfa062dfcfe
Roman Eskin <r.eskin@arenadata.io>
2024-11-19 09:59:22 +1000

    Rework gp_optimizer functions to use runtime check of Orca's presence

--- src/backend/utils/adt/gp_optimizer_functions.c
+++ src/backend/utils/adt/gp_optimizer_functions.c
@@ -16,7 +16,44 @@
 #include "funcapi.h"
 #include "utils/builtins.h"
 
-extern Datum EnableXform(PG_FUNCTION_ARGS);
+typedef Datum (*gp_opt_version_func) (void);
+
+/*
+ * Loads optimizer function from a shared library. If library is not presented
+ * or doesn't contain the requested function or any of its dependencies,
+ * returns NULL.
+ */
+static PGFunction
+gp_optimizer_load_function(char *funcname)
+{
+	volatile PGFunction func = NULL;
+	int32		savedInterruptHoldoffCount = InterruptHoldoffCount;
+	MemoryContext oldcontext = CurrentMemoryContext;
+
+	PG_TRY();
+	{
+		func = load_external_function("$libdir/gp_orca", funcname, false, NULL);
+	}
+	PG_CATCH();
+	{
+		MemoryContextSwitchTo(oldcontext);
+		InterruptHoldoffCount = savedInterruptHoldoffCount;
+		FlushErrorState();
+		func = NULL;
+	}
+	PG_END_TRY();
+
+	return func;
+}
+
+/*
+ * A stub function to call if no optimizer function is found.
+ */
+static Datum
+gp_optimizer_function_stub()
+{
+	return CStringGetTextDatum("Server has been compiled without ORCA");
+}
 
 /*
 * Enables transformations in the optimizer.
@@ -24,39 +61,39 @@ extern Datum EnableXform(PG_FUNCTION_ARGS);
 Datum
 enable_xform(PG_FUNCTION_ARGS)
 {
-#ifdef USE_ORCA
-	return EnableXform(fcinfo);
-#else
-	return CStringGetTextDatum("Server has been compiled without ORCA");
-#endif
+	PGFunction	func = gp_optimizer_load_function("EnableXform");
+
+	if (func)
+		return func(fcinfo);
+	else
+		return gp_optimizer_function_stub();
 }
 
-extern Datum DisableXform(PG_FUNCTION_ARGS);
-
-/* 
+/*
 * Disables transformations in the optimizer.
 */
 Datum
 disable_xform(PG_FUNCTION_ARGS)
 {
-#ifdef USE_ORCA
-	return DisableXform(fcinfo);
-#else
-	return CStringGetTextDatum("Server has been compiled without ORCA");
-#endif
+	PGFunction	func = gp_optimizer_load_function("DisableXform");
+
+	if (func)
+		return func(fcinfo);
+	else
+		return gp_optimizer_function_stub();
 }
 
-extern Datum LibraryVersion();
-	
 /*
 * Returns the optimizer and gpos library versions.
 */
 Datum
 gp_opt_version(PG_FUNCTION_ARGS pg_attribute_unused())
 {
-#ifdef USE_ORCA
-	return LibraryVersion();
-#else
-	return CStringGetTextDatum("Server has been compiled without ORCA");
-#endif
+	gp_opt_version_func func =
+		(gp_opt_version_func) gp_optimizer_load_function("LibraryVersion");
+
+	if (func)
+		return func();
+	else
+		return gp_optimizer_function_stub();
 }
