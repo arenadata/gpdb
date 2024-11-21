5d425a7053e36a0218ba570a8c821662818d7c80
Maksim Michkov <m.michkov@arenadata.io>
2024-11-21 10:11:54 +0000

    Update comment

--- src/test/isolation2/sql/vacuum_progress_column.sql
+++ src/test/isolation2/sql/vacuum_progress_column.sql
@@ -140,7 +140,8 @@ select relid::regclass as relname, phase, heap_blks_total, heap_blks_scanned, he
 -- of post-cleanup taken over by a new vacuum worker.
 2: SELECT gp_inject_fault('vacuum_worker_changed', 'suspend', dbid) FROM gp_segment_configuration WHERE content > -1 AND role = 'p';
 
--- we shouldn't proceed until FTS probe has bumped the version
+-- Dispatcher shouldn't proceed until status_version in FtsVersion in FtsProbeInfo is updated, 
+-- ensuring the backends are restarted before they start the post-cleanup phase.
 2: SELECT gp_inject_fault('vacuum_rel_finished_one_relation', 'suspend', '', '', 'vacuum_progress_ao_column', 1, 1, 0, 1) FROM master();
 -- resume walsender and let it exit so that mirror stop can be detected
 2: SELECT gp_inject_fault_infinite('wal_sender_loop', 'reset', dbid) FROM gp_segment_configuration WHERE role = 'p' and content = 1;
