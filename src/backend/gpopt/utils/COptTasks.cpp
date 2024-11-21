cbdd64bb93caf36b1471aa06cfd41a2f3591f460
Denis Kovalev <d.kovalev@arenadata.io>
2024-11-21 17:47:09 +0500

    Remove unneeded type cast

--- src/backend/gpopt/utils/COptTasks.cpp
+++ src/backend/gpopt/utils/COptTasks.cpp
@@ -570,7 +570,7 @@ COptTasks::GetPlanHints(CMemoryPool *mp, Query *query)
 	{
 		// Calling plan_hint_hook creates pg_hint_plan hint structures
 		// (see optimizer/hints.h).
-		hintstate = (HintState *) plan_hint_hook(query);
+		hintstate = plan_hint_hook(query);
 	}
 
 	if (nullptr == hintstate)
