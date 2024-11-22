ee7b36b446712bd791f856526f04485206d6e75b
Roman Eskin <r.eskin@arenadata.io>
2024-11-22 14:22:57 +1000

    Update message

--- src/backend/utils/adt/gp_optimizer_functions.c
+++ src/backend/utils/adt/gp_optimizer_functions.c
@@ -24,7 +24,7 @@ typedef Datum (*gp_opt_version_func) (void);
 static Datum
 gp_optimizer_function_stub(PG_FUNCTION_ARGS)
 {
-	return CStringGetTextDatum("ORCA is not added to 'shared_preload_libraries'");
+	return CStringGetTextDatum("gp_orca is not added to 'shared_preload_libraries'");
 }
 
 /*
