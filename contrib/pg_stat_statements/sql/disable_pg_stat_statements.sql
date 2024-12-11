41106db9f7eba29e3fd9b20c1458a7c55b658af9
Roman Eskin <r.eskin@arenadata.io>
2024-12-11 17:59:01 +1000

    Fix pg_stat_statements setup and teardown

--- contrib/pg_stat_statements/sql/disable_pg_stat_statements.sql
+++ contrib/pg_stat_statements/sql/disable_pg_stat_statements.sql
@@ -1,4 +1,4 @@
 -- start_ignore
-\! gpconfig -r shared_preload_libraries;
+\! gpconfig -c shared_preload_libraries -v "$(psql -At -c "SELECT array_to_string(array_remove(string_to_array(current_setting('shared_preload_libraries'), ','), 'pg_stat_statements'), ',')" postgres)"
 \! gpstop -raq -M fast;
 -- end_ignore
