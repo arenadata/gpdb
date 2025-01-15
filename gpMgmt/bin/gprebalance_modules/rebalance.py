81e3755befa1edd0045e740340a31d0048588154
Alexander Kondakov <a.kondakov@arenadata.io>
2025-01-15 08:30:16 +0300

    generalize hosts work

--- gpMgmt/bin/gprebalance_modules/rebalance.py
+++ gpMgmt/bin/gprebalance_modules/rebalance.py
@@ -1,7 +1,7 @@
 import yaml
 import os
 from dataclasses import dataclass
-from typing import List, Set
+from typing import List, Set, Dict
 from enum import Enum
 from gppylib.db import dbconn
 from gppylib.gparray import GpArray, Segment, MODE_NOT_SYNC, STATUS_DOWN
@@ -24,45 +24,6 @@ class MirrorStrategy(Enum):
     SPREAD = "spread"
 
 
-def form_target_hosts(gparray: GpArray, filename: str) -> List[Host]:
-    hosts = {}
-    for seg in gparray.getSegmentsAsLoadedFromDb():
-        if seg.content >= 0:
-            hosts[(seg.hostname, seg.address)] = Host(
-                hostname=seg.hostname, address=seg.address, primary_datadirs=set(), mirror_datadirs=set(),
-                primary_segments=set(), mirror_segments=set())
-    for pair in gparray.segmentPairs:
-        primary = pair.primaryDB
-        mirror = pair.mirrorDB
-        key_pr = (primary.hostname, primary.address)
-        hosts[key_pr].primary_datadirs.add(os.path.dirname(primary.datadir))
-        hosts[key_pr].primary_segments.add(primary.content)
-        if mirror:
-            key_mr = (mirror.hostname, mirror.address)
-            hosts[key_mr].mirror_datadirs.add(os.path.dirname(mirror.datadir))
-            hosts[key_mr].mirror_segments.add(mirror.content)
-    if filename:
-        with open(filename, 'r') as fp:
-            config = yaml.safe_load(fp)
-            for host_config in config['hosts']:
-                key = (host_config['hostname'], host_config['address'])
-                if key not in hosts:
-                    hosts[key] = Host(hostname=host_config['hostname'],
-                                      address=host_config['address'],
-                                      primary_datadirs=set(
-                                          host_config['primary_datadirs']),
-                                      mirror_datadirs=set(
-                                          host_config['mirror_datadirs']),
-                                      primary_segments=set(), mirror_segments=set())
-                else:
-                    hosts[key].primary_datadirs.union(
-                        set(host_config['primary_datadirs']))
-                    hosts[key].mirror_datadirs.union(
-                        set(host_config['mirror_datadirs']))
-
-    return list(hosts.values())
-
-
 class GPRebalance:
     def __init__(self, logger, gparray, dburl, options):
         self.logger = logger
@@ -71,7 +32,6 @@ class GPRebalance:
         self.original_gparray = gparray
         self.conn = dbconn.connect(
             self.dburl, utility=True, encoding='UTF8', allowSystemTableMods=True)
-        self.target_hosts = form_target_hosts(gparray, options.filename)
         if options.mirroring == 'spread':
             self.target_strategy = MirrorStrategy.SPREAD
         elif options.mirroring == 'grouped':
@@ -79,5 +39,47 @@ class GPRebalance:
         else:
             self.target_strategy = MirrorStrategy.MIRRORLESS
 
+        hosts = self.getHostsFromGpArray()
+        if options.filename:
+            with open(options.filenam, 'r') as fp:
+                config = yaml.safe_load(fp)
+                for host_config in config['hosts']:
+                    key = (host_config['hostname'], host_config['address'])
+                    if key not in hosts:
+                        hosts[key] = Host(hostname=host_config['hostname'],
+                                          address=host_config['address'],
+                                          primary_datadirs=set(
+                                          host_config['primary_datadirs']),
+                                          mirror_datadirs=set(
+                                          host_config['mirror_datadirs']),
+                                          primary_segments=set(), mirror_segments=set())
+                    else:
+                        hosts[key].primary_datadirs.union(
+                            set(host_config['primary_datadirs']))
+                        hosts[key].mirror_datadirs.union(
+                            set(host_config['mirror_datadirs']))
+        self.target_hosts = list(hosts.values())
+
     def setMirroringStrategy(self, strategy: MirrorStrategy):
         self.target_strategy = strategy
+
+    def getHostsFromGpArray(self) -> Dict[tuple[str, str], Host]:
+        hosts = {}
+        for seg in self.original_gparray.getSegmentsAsLoadedFromDb():
+            if seg.content >= 0:
+                hosts[(seg.hostname, seg.address)] = Host(
+                    hostname=seg.hostname, address=seg.address, primary_datadirs=set(), mirror_datadirs=set(),
+                    primary_segments=set(), mirror_segments=set())
+        for pair in self.original_gparray.segmentPairs:
+            primary = pair.primaryDB
+            mirror = pair.mirrorDB
+            key_pr = (primary.hostname, primary.address)
+            hosts[key_pr].primary_datadirs.add(
+                os.path.dirname(primary.datadir))
+            hosts[key_pr].primary_segments.add(primary.content)
+            if mirror:
+                key_mr = (mirror.hostname, mirror.address)
+                hosts[key_mr].mirror_datadirs.add(
+                    os.path.dirname(mirror.datadir))
+                hosts[key_mr].mirror_segments.add(mirror.content)
+        return hosts
