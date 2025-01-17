dd090850b0976dc6a26f7dd52aa74a4c5a15b6ff
Georgy Shelkovy <g.shelkovy@arenadata.io>
2025-01-17 09:28:33 +0500

    Don't send archival report in archive_mode always

--- src/backend/replication/walsender.c
+++ src/backend/replication/walsender.c
@@ -1895,7 +1895,7 @@ WalSndComputeSleeptime(TimestampTz now)
 	}
 
 	/* If archiving is enabled, send a status report to the client */
-	if (XLogArchivingStatusReportingActive())
+	if (XLogArchivingStatusReportingActive() && XLogArchiveMode != ARCHIVE_MODE_ALWAYS)
 	{
 		/*
 		 * If we requested an update from pgstat, poll every
@@ -3505,7 +3505,7 @@ WalSndKeepaliveIfNecessary(void)
 	/*
 	 * Send an archival status message, if necessary.
 	 */
-	if (XLogArchivingStatusReportingActive())
+	if (XLogArchivingStatusReportingActive() && XLogArchiveMode != ARCHIVE_MODE_ALWAYS)
 		WalSndArchivalReportIfNecessary(GetCurrentTimestamp());
 
 	/*
