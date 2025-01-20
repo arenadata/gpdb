af128be8946616a8820829d25c1caa32faeb72ef
Evgeniy Ratkov <e.ratkov@arenadata.io>
2025-01-20 15:17:11 +0300

    update comment

--- src/backend/optimizer/path/allpaths.c
+++ src/backend/optimizer/path/allpaths.c
@@ -754,7 +754,9 @@ set_rel_pathlist(PlannerInfo *root, RelOptInfo *rel,
 	 * Greenplum specific behavior:
 	 * Change the path in pathlist if it is a general or segmentgeneral
 	 * path that contains volatile restrictions.
-	 * Do not do it, if there is outer query.
+	 * Do not do it, if list of the outer quals is not empty.
+	 * Outer query's motion will be decorated with materialize by
+	 * bring_to_outer_query.
 	 */
 	if (rel->upperrestrictinfo == NULL && rel->reloptkind == RELOPT_BASEREL)
 		handle_gen_seggen_volatile_path(root, rel);
