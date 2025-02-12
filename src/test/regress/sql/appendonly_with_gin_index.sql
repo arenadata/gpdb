e1da68fa1e8b0ad3de8a56b40f0c2986d2062e0c
Aleksandr Kopytov <a.kopytov@arenadata.io>
2025-02-12 18:40:41 +0300

    Fix assert when using append optimized tables with GIN index (#1221)
    Using an AO table with a GIN index could cause an assert in the debug build.
    The triggered assert (OffsetNumberIsValid) checks the offset (ip_posid) in the
    tuple address (ItemPointerData), indirectly checking the correctness of
    GinPostingList decompression. Decompression can be performed, for example,
    when inserting data into an AO table with a GIN index or starting a vacuum.
    The offset range is checked as for a Heap table. The correct range is 1..8192
    with a BLCKSZ of 32768 bytes (see MaxOffsetNumber).
    However, in AO tables, the TID offset (ip_posid) is calculated differently,
    this led to the assert.
    
    In a Heap table, ip_posid means the ItemIdData number inside the PageHeader
    structure. ip_posid for Heap tables is calculated by PageGetMaxOffsetNumber()
    macro, for example during insertion. ip_posid can take the value 1..8192 with
    BLCKSZ size 32768 bytes.
    
    For AO tables ip_posid is calculated as the value of the lower 16 bits of the
    segfile row in AOTupleIdInit() and can take the value 1..32768. What could
    cause the assert.
    
    A larger range of ip_posid with AO tuples does not cause problems in the GIN
    index, since the full tuple address (ItemPointerData) is used during
    processing.
    
    This commit weakens the offset range check (ip_posid) in the assert at the
    GinPostingList decompression stage so that AO tables with GIN index can be
    processed in the build with asserts.
    
    Also added a test.

--- src/test/regress/sql/appendonly_with_gin_index.sql
+++ src/test/regress/sql/appendonly_with_gin_index.sql
@@ -1,19 +1,25 @@
--- Given I have an append-only table
-create table users(
-  first_name tsvector
-) with (appendonly=true);
+-- check that GIN index works with large TID offset (second ctid field) when using AO table
+-- Orca performs seq scan in this case, so disable
+set optimizer = 0;
+set enable_seqscan = 0;
+-- collect all tuples on one segment
+create temp table t1 (a text, b int default 0) with (appendonly=true) distributed by (b);
+insert into t1 select MD5(random()::text) from generate_series(1, 500000);
+insert into t1 values ('apple');
+create index t1_idx on t1 using gin(to_tsvector('english', a)) with (fastupdate = false);
 
--- And I have a large amount of data in the table
-insert into users
-  select to_tsvector(md5(random()::text))
-  from generate_series(1, 6000) i;
+-- divide block_size by 4 because the size of ItemIdData is 4 bytes
+with query as (select a, (ctid::text::point)[1]::int as offset from t1
+              where to_tsvector('english', a) @@ to_tsquery('english', 'apple')),
+     maxHeapOffset as (select current_setting('block_size')::int/4 as value)
+select a from query as q where q.offset > (select * from maxHeapOffset);
 
-insert into users values ('John');
+explain (costs off)
+with query as (select a, (ctid::text::point)[1]::int as offset from t1
+              where to_tsvector('english', a) @@ to_tsquery('english', 'apple')),
+     maxHeapOffset as (select current_setting('block_size')::int/4 as value)
+select a from query as q where q.offset > (select * from maxHeapOffset);
 
--- When I create a GIN index on users
-set gp_debug_linger=1;
-CREATE INDEX users_search_idx ON users USING gin (first_name);
-reset gp_debug_linger;
-
--- Then I should be able to query the table
-select * from users where first_name = 'John';
+drop table t1;
+reset enable_seqscan;
+reset optimizer;
