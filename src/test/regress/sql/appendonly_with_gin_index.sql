da25fe1c6149324fde64f8199d7ab89a322213f7
Aleksandr Kopytov <a.kopytov@arenadata.io>
2025-02-17 10:01:20 +0300

    Use the original version of the test with corrections

--- src/test/regress/sql/appendonly_with_gin_index.sql
+++ src/test/regress/sql/appendonly_with_gin_index.sql
@@ -1,30 +1,26 @@
--- Check that GIN index works with large TID offset (second ctid field) when using AO table.
+-- Given I have an append-only table
+create table users(
+  first_name tsvector
+) with (appendonly=true);
+
+-- And I have a large amount of data in the table
+insert into users
+  select to_tsvector( md5(random()::text))
+  from generate_series(1, 60000) i;
+
+insert into users values (to_tsvector('John'));
+
+-- When I create a GIN index on users
+set gp_debug_linger=1;
+CREATE INDEX users_search_idx ON users USING gin (first_name);
+reset gp_debug_linger;
+
 -- Orca performs seq scan in this case, so disable Orca.
 set optimizer = 0;
-set enable_seqscan = 0;
--- Collect all tuples on one segment.
-create temp table t1 (a text, b int default 0) with (appendonly=true) distributed by (b);
-insert into t1 select MD5(random()::text) from generate_series(1, 50000);
-insert into t1 values ('apple');
-create index t1_idx on t1 using gin(to_tsvector('english', a)) with (fastupdate = false);
 
--- We define the maximum TID offset for a Heap table in the maxHeapOffset query as
--- the maximum block size (BLCKSZ) divided by the pointer size (4 bytes), similar
--- to the MaxOffsetNumber macro. But we are working with an AO table, in which a
--- tuple may have a larger offset. To make the offset of the AO table tuple exceed
--- maxHeapOffset, need to add enough data. By performing an index scan on the query,
--- we ensure that a tuple with a large offset can be processed by a GIN index.
-with query as (select a, (ctid::text::point)[1]::int as offset from t1
-              where to_tsvector('english', a) @@ to_tsquery('english', 'apple')),
-     maxHeapOffset as (select current_setting('block_size')::int/4 as value)
-select a from query as q where q.offset > (select * from maxHeapOffset);
+-- Then I should be able to query the table
+select * from users where first_name @@ to_tsquery('John');
+explain select * from users where first_name @@ to_tsquery('John');
 
-explain (costs off)
-with query as (select a, (ctid::text::point)[1]::int as offset from t1
-              where to_tsvector('english', a) @@ to_tsquery('english', 'apple')),
-     maxHeapOffset as (select current_setting('block_size')::int/4 as value)
-select a from query as q where q.offset > (select * from maxHeapOffset);
-
-drop table t1;
-reset enable_seqscan;
+drop table users;
 reset optimizer;
