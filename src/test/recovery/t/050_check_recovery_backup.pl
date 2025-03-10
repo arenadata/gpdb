7be811fa99accdd20afa04f89ad63c04d22b8274
Evgeniy Ratkov <e.ratkov@arenadata.io>
2025-03-10 15:53:07 +0300

    expand test

--- src/test/recovery/t/050_check_recovery_backup.pl
+++ src/test/recovery/t/050_check_recovery_backup.pl
@@ -37,10 +37,10 @@ sub create_backup_primary
 
 sub restore_backup_primary
 {
-	my ($backup_dir, $restore_point, $label) = @_;
+	my ($backup_dir, $restore_options, $label) = @_;
 	my $primary_pgdata = $primary->data_dir;
 
-	print("Running: restore_backup_primary: '$restore_point' '$label'\n");
+	print("Running: restore_backup_primary: '$restore_options' '$label'\n");
 
 	$primary->command_ok([ 'rm', '-rf', $primary_pgdata ],
 		"'$label': rm primary pgdata");
@@ -52,7 +52,7 @@ sub restore_backup_primary
 	open my $conf, '>', "$primary_pgdata/postgresql.auto.conf";
 	print $conf "recovery_target_action = 'promote'\n";
 	print $conf "restore_command = 'cp -vr $archivedir/%f $primary_pgdata/%p'\n";
-	print $conf "recovery_target_name = '$restore_point'\n";
+	print $conf "$restore_options\n";
 	close $conf;
 	$primary->start;
 }
@@ -80,24 +80,38 @@ $primary->safe_psql('postgres', 'CREATE ROLE postgres WITH LOGIN REPLICATION');
 
 create_backup_primary($basebackupdir1, 'backup_label1');
 $primary->stop('smart');
-restore_backup_primary($basebackupdir1, 'backup_label1', '001 -> 012');
+restore_backup_primary($basebackupdir1, "recovery_target_name = 'backup_label1'", '001 -> 012');
 wait_recovery_and_switch_wal_primary('001 -> 012');
 
 create_backup_primary($basebackupdir2, 'backup_label2');
 $primary->stop('smart');
-restore_backup_primary($basebackupdir2, 'backup_label2', '012 -> 013');
+restore_backup_primary($basebackupdir2, "recovery_target_name = 'backup_label2'", '012 -> 013');
 wait_recovery_and_switch_wal_primary('012 -> 013');
 
 $primary->stop('smart');
-restore_backup_primary($basebackupdir2, 'backup_label2', '012 -> 023');
+restore_backup_primary($basebackupdir2, "recovery_target_name = 'backup_label2'", '012 -> 023');
 wait_recovery_and_switch_wal_primary('012 -> 023');
 
 $primary->stop('smart');
-restore_backup_primary($basebackupdir1, 'backup_label1', '101 -> 112');
+restore_backup_primary($basebackupdir1, "recovery_target_name = 'backup_label1'", '101 -> 112');
 wait_recovery_and_switch_wal_primary('101 -> 112');
 
 $primary->stop('smart');
-restore_backup_primary($basebackupdir2, 'backup_label2', '112 -> 113');
+restore_backup_primary($basebackupdir2, "recovery_target_name = 'backup_label2'", '112 -> 113');
 wait_recovery_and_switch_wal_primary('112 -> 113');
+
+$primary->safe_psql('test_db', 'CREATE TABLE test_2 as select generate_series(1,10)');
+$primary->safe_psql('test_db', 'SELECT * FROM test_2');
+$primary->safe_psql('test_db', 'CHECKPOINT');
+
+$primary->stop('smart');
+restore_backup_primary($basebackupdir2, "recovery_target_timeline = 6", '113 -> 114');
+wait_recovery_and_switch_wal_primary('latest');
+$primary->safe_psql('test_db', 'SELECT * FROM test_2');
+
+$primary->stop('smart');
+restore_backup_primary($basebackupdir2, "recovery_target_timeline = 'latest'", '113 -> 115');
+wait_recovery_and_switch_wal_primary('latest');
+$primary->safe_psql('test_db', 'SELECT * FROM test_2');
 
 done_testing();
