bed16f14c37bb6c01f29ddd10916af40df7e780b
kotb <zavgmaxden@mail.ru>
2025-03-11 23:17:33 +0300

    revert comment edit

--- src/test/regress/sql/with_clause.sql
+++ src/test/regress/sql/with_clause.sql
@@ -442,7 +442,8 @@ with cte as (
 ) select count(*) from cte where i > 2;
 select count(*) c from with_dml;
 
--- Test ORCA shares DML CTE results if multiple CTE references found.
+-- Test one cannot use DML CTE if multiple CTE references found.
+-- Otherwise it will cause duplicated DML operations or planner errors.
 explain (costs off)
 with cte as (
     insert into with_dml select i, i * 100 from generate_series(1,5) i
