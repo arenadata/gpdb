07a200243aec0f5c14137f6f11d441bc0311c980
Georgy Shelkovy <g.shelkovy@arenadata.io>
2025-03-13 09:14:50 +0500

    Fix missing argument when calling smgropen
    Commit e21856f added a new function RelationGetSmgr to src/include/utils/rel.h,
    which called smgropen with two arguments, while the earlier commit 19cd1cf had
    already added a third argument to this function. This patch fixes this
    inconsistency by adding a third argument when calling smgropen in
    RelationGetSmgr, as is done in the similar deprecated macro RelationOpenSmgr.

--- src/include/utils/rel.h
+++ src/include/utils/rel.h
@@ -579,7 +579,8 @@ static inline SMgrRelation
 RelationGetSmgr(Relation rel)
 {
 	if (unlikely(rel->rd_smgr == NULL))
-		smgrsetowner(&(rel->rd_smgr), smgropen(rel->rd_node, rel->rd_backend));
+		smgrsetowner(&(rel->rd_smgr), smgropen(rel->rd_node, rel->rd_backend,
+					 RelationStorageIsAO(rel) ? SMGR_AO : SMGR_MD));
 	return rel->rd_smgr;
 }
 #endif							/* !FRONTEND */
