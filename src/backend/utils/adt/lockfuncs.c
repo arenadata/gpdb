e1fd141a51ff66d457510e9932bc881a56eb3685
Vladimir Sarmin <v.sarmin@arenadata.io>
2025-03-31 10:12:41 +0300

    Revert "large synthetic locks"
    This reverts commit 9924a5cbdca829255c6468a5565174460b0e21e9.

--- src/backend/utils/adt/lockfuncs.c
+++ src/backend/utils/adt/lockfuncs.c
@@ -84,7 +84,7 @@ VXIDGetDatum(BackendId bid, LocalTransactionId lxid)
  * distribution on it.
  */
 #define PG_LOCKS_INTERNAL_QUERY \
-	"select l.* from gp_dist_random('pg_catalog.pg_locks') l, generate_series(1, 10000)"
+	"select * from gp_dist_random('pg_catalog.pg_locks')"
 
 /*
  * Build a querydesc for SELECT on pg_locks relation to be passed to executor.
