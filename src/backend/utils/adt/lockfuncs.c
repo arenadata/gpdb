9924a5cbdca829255c6468a5565174460b0e21e9
Vladimir Sarmin <v.sarmin@arenadata.io>
2025-03-31 09:59:38 +0300

    large synthetic locks

--- src/backend/utils/adt/lockfuncs.c
+++ src/backend/utils/adt/lockfuncs.c
@@ -84,7 +84,7 @@ VXIDGetDatum(BackendId bid, LocalTransactionId lxid)
  * distribution on it.
  */
 #define PG_LOCKS_INTERNAL_QUERY \
-	"select * from gp_dist_random('pg_catalog.pg_locks')"
+	"select l.* from gp_dist_random('pg_catalog.pg_locks') l, generate_series(1, 10000)"
 
 /*
  * Build a querydesc for SELECT on pg_locks relation to be passed to executor.
