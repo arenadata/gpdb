6440bf301aa0c53279bae88c137d4a88aa151875
Georgy Shelkovy <g.shelkovy@arenadata.io>
2025-04-23 07:59:44 +0500

    use return value

--- src/backend/cdb/cdbutil.c
+++ src/backend/cdb/cdbutil.c
@@ -929,7 +929,8 @@ cdbcomponent_recycleIdleQE(SegmentDatabaseDescriptor *segdbDesc, bool forceDestr
 			 lastWriter = cell, cell = lnext(segdbDesc->segment_database_info->freelist, cell)) ;
 
 		if (lastWriter)
-			lappend(segdbDesc->segment_database_info->freelist, segdbDesc);
+			segdbDesc->segment_database_info->freelist =
+				lappend(segdbDesc->segment_database_info->freelist, segdbDesc);
 		else
 			segdbDesc->segment_database_info->freelist =
 				lcons(segdbDesc, segdbDesc->segment_database_info->freelist);
