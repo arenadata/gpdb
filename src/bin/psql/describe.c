a2a09faf56536491379a285f9483246739657af6
Georgy Shelkovy <g.shelkovy@arenadata.io>
2025-04-23 16:55:17 +0500

    fix

--- src/bin/psql/describe.c
+++ src/bin/psql/describe.c
@@ -4623,7 +4623,7 @@ listTables(const char *tabtypes, const char *pattern, bool verbose, bool showSys
 	PGresult   *res;
 	printQueryOpt myopt = pset.popt;
 	int			cols_so_far;
-	bool		translate_columns[] = {false, false, true, false, false /* Storage */, false, false, false, false};
+	bool		translate_columns[] = {false, false, true, false, false /* Storage */, false, false, false, false, false};
 
 	/* If tabtypes is empty, we default to \dtvmsE (but see also command.c) */
 	if (!(showTables || showIndexes || showViews || showMatViews || showSeq || showForeign))
