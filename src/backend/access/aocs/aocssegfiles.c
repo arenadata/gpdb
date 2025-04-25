8256912dfa43fa5b50c89bb98fcd2b09e7795324
Ernest <ernest_aleksandrov@mail.ru>
2025-04-25 20:48:02 +0300

    Add missed volatile qualifier
    The connected variable is modified in the PG_TRY block and used in the PG_CATCH
    block. It should be declared as volatile

--- src/backend/access/aocs/aocssegfiles.c
+++ src/backend/access/aocs/aocssegfiles.c
@@ -1512,7 +1512,7 @@ gp_update_aocol_master_stats_internal(Relation parentrel, Snapshot appendOnlyMet
 {
 	StringInfoData sqlstmt;
 	Relation	aosegrel;
-	bool		connected = false;
+	volatile bool connected = false;
 	int			proc;	/* 32 bit, only holds number of segments */
 	int			ret;
 	int64		total_count = 0;
@@ -1698,7 +1698,7 @@ aocol_compression_ratio_internal(Relation parentrel)
 {
 	StringInfoData sqlstmt;
 	Relation	aosegrel;
-	bool		connected = false;
+	volatile bool connected = false;
 	int			proc;	/* 32 bit, only holds number of segments */
 	int			ret;
 	int64		eof = 0;
