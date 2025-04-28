7b6daae70f2af6e4578f2a49fd55cb8dc1009a52
Evgeniy Ratkov <e.ratkov@arenadata.io>
2025-04-28 17:16:12 +0300

    fix comment

--- src/backend/gporca/libgpopt/src/translate/CTranslatorExprToDXL.cpp
+++ src/backend/gporca/libgpopt/src/translate/CTranslatorExprToDXL.cpp
@@ -3908,7 +3908,7 @@ CTranslatorExprToDXL::PdxlnCorrelatedNLJoin(
 	// select * from foo where 10 > (select bar.b from bar where foo.b = bar.b);
 	// If bar is a very small table, ORCA generates a CorrelatedInnerNLJoin with
 	// a Const true join filter and condition 10 > bar.b is added as a filter
-	// to the  scan of bar.
+	// to the scan of bar.
 	// For the other cases function BuildSubplans generates another sublink type
 	// for correct type of join condition.
 	if (CUtils::FScalarConstTrue(pexprScalar) &&
