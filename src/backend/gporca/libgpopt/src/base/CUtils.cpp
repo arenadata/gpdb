85504515b7a0711beab2f1cb7650780579452d1f
Maksim Michkov <m.michkov@arenadata.io>
2025-05-16 07:14:33 +0000

    Fix assert condition

--- src/backend/gporca/libgpopt/src/base/CUtils.cpp
+++ src/backend/gporca/libgpopt/src/base/CUtils.cpp
@@ -2093,7 +2093,7 @@ CUtils::FHasCountAggMatchingColumn(const CExpression *pexpr,
 				if (nullptr != pdrgpexprPredicates &&
 					COperator::EopLogicalSelect == pop->Eopid())
 				{
-					GPOS_ASSERT(arity == 2);
+					GPOS_ASSERT(2 == arity);
 					CExpression *pexprPred = (*pexpr)[1];
 					pexprPred->AddRef();
 					pdrgpexprPredicates->Append(pexprPred);
