0d80fb552a2da9f03e7cb38d59bde32031ce711c
Georgy Shelkovy <g.shelkovy@arenadata.io>
2025-05-19 16:15:59 +0500

    sort

--- src/backend/commands/analyzefuncs.c
+++ src/backend/commands/analyzefuncs.c
@@ -1,8 +1,8 @@
 #include "postgres.h"
 
 #include "access/aocssegfiles.h"
-#include "access/table.h"
 #include "access/heaptoast.h"
+#include "access/table.h"
 #include "catalog/pg_appendonly.h"
 #include "catalog/pg_type.h"
 #include "cdb/cdbappendonlyam.h"
