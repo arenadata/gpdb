93f4bf1dc9c7a55e08984f2e1d686e0c34f3ca32
Georgy Shelkovy <g.shelkovy@arenadata.io>
2025-05-23 08:25:01 +0500

    Fix conflicts in isolationtester.h, isolationtester.c and specparse.y (#1603)
    1) Commit 989d23b added a new field used to the Step structure, while the
    earlier commit bbdc17b had already moved this and the session field to the end
    of the structure.
    
    2) Commit 9903338 removed the global variable dry_run, while the earlier commit
    5165315 added a new global variable max_step_wait before this place.
    
    3) Commit bbdc17b moved some code from the main function to a new function
    check_testspec, and also changed the memory allocation at the beginning of the
    run_permutation function.
    
    4) Commit 989d23b added initialization of the new used field, while the earlier
    commit bbdc17b added initialization of the session field before that and
    removed the initialization of the errormsg field.
    
    5) Commit 989d23b added a loop to the run_testspec function, while an earlier
    commit bbdc17b had already removed it.
    
    6) Commit 989d23b added a used field setting to the run_permutation function,
    while an earlier commit bbdc17b had already removed it.

--- src/test/isolation/isolationtester.h
+++ src/test/isolation/isolationtester.h
@@ -32,11 +32,6 @@ typedef struct
 
 struct Step
 {
-<<<<<<< HEAD
-=======
-	int			session;
-	bool		used;
->>>>>>> eb57bd9c1d83a20eaff559a53b2f584dcd0668a8
 	char	   *name;
 	char	   *sql;
 	/* These fields are filled by check_testspec(): */
