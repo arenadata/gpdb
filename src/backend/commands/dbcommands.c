080e393792e28d278b82698caeb9403f5a8236b6
Viktor Kurilko <v.kurilko@arenadata.io>
2025-06-23 11:20:33 +0700

    Resolve conflicts in the src/backend/commands/dbcommands.c (#1734)
    Commit 9fab25c6cd1f943284b8e0014007cd5750d54308 moved check for other backends,
    while earlier commit 6b0e52beadd678c5050af9c978c26d171ba86ae0 added gpdb
    specific code nearby.

--- src/backend/commands/dbcommands.c
+++ src/backend/commands/dbcommands.c
@@ -1009,7 +1009,19 @@ dropdb(const char *dbname, bool missing_ok)
 								  nsubscriptions, nsubscriptions)));
 
 	/*
-<<<<<<< HEAD
+	 * Check for other backends in the target database.  (Because we hold the
+	 * database lock, no new ones can start after this.)
+	 *
+	 * As in CREATE DATABASE, check this after other error conditions.
+	 */
+	if (CountOtherDBBackends(db_id, &notherbackends, &npreparedxacts))
+		ereport(ERROR,
+				(errcode(ERRCODE_OBJECT_IN_USE),
+						errmsg("database \"%s\" is being accessed by other users",
+							   dbname),
+						errdetail_busy_db(notherbackends, npreparedxacts)));
+
+	/*
 	 * Free the database on the segDBs
 	 */
 	if (Gp_role == GP_ROLE_DISPATCH)
@@ -1030,19 +1042,6 @@ dropdb(const char *dbname, bool missing_ok)
 							NULL);
 		pfree(buffer.data);
 	}
-=======
-	 * Check for other backends in the target database.  (Because we hold the
-	 * database lock, no new ones can start after this.)
-	 *
-	 * As in CREATE DATABASE, check this after other error conditions.
-	 */
-	if (CountOtherDBBackends(db_id, &notherbackends, &npreparedxacts))
-		ereport(ERROR,
-				(errcode(ERRCODE_OBJECT_IN_USE),
-				 errmsg("database \"%s\" is being accessed by other users",
-						dbname),
-				 errdetail_busy_db(notherbackends, npreparedxacts)));
->>>>>>> BISECT_HEAD
 
 	/*
 	 * Remove the database's tuple from pg_database.
