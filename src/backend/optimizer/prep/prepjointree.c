2442a5b7a1f353a461fdcafc4b2d1c5f2a6b7114
Alexander Kondakov <a.kondakov@arenadata.io>
2025-07-02 08:51:49 +0300

    remove declaration

--- src/backend/optimizer/prep/prepjointree.c
+++ src/backend/optimizer/prep/prepjointree.c
@@ -88,8 +88,6 @@ static void pull_up_union_leaf_queries(Node *setOp, PlannerInfo *root,
 									   int childRToffset);
 static void make_setop_translation_list(Query *query, Index newvarno,
 										AppendRelInfo *appinfo);
-static bool is_simple_subquery(Query *subquery, RangeTblEntry *rte,
-							   JoinExpr *lowest_outer_join);
 static Node *pull_up_simple_values(PlannerInfo *root, Node *jtnode,
 								   RangeTblEntry *rte);
 static bool is_simple_values(PlannerInfo *root, RangeTblEntry *rte);
