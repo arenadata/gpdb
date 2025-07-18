70641a5a55e8ca9a586ef5c64aa33d45a434ab85
Georgy Shelkovy <g.shelkovy@arenadata.io>
2025-07-18 13:54:06 +0500

    fix

--- src/backend/cdb/cdbsubselect.c
+++ src/backend/cdb/cdbsubselect.c
@@ -814,7 +814,7 @@ mutate_targetlist(List *tlist)
 static int
 add_notin_subquery_rte(Query *parse, Query *subselect)
 {
-	RangeTblEntry *subq_rte;
+	ParseNamespaceItem *subq_nsitem;
 	int			subq_indx;
 
 	/*
@@ -823,16 +823,16 @@ add_notin_subquery_rte(Query *parse, Query *subselect)
 	 * refer to other RTEs in the parent query.
 	 */
 	subselect->targetList = mutate_targetlist(subselect->targetList);
-	subq_rte = addRangeTableEntryForSubquery(NULL,	/* pstate */
+	subq_nsitem = addRangeTableEntryForSubquery(NULL,	/* pstate */
 											 subselect,
 											 makeAlias("NotIn_SUBQUERY", NIL),
 											 false, /* not lateral */
 											 false /* inFromClause */ );
-	parse->rtable = lappend(parse->rtable, subq_rte);
+	parse->rtable = lappend(parse->rtable, subq_nsitem->p_rte);
 
 	/* assume new rte is at end */
 	subq_indx = list_length(parse->rtable);
-	Assert(subq_rte == rt_fetch(subq_indx, parse->rtable));
+	Assert(subq_nsitem->p_rte == rt_fetch(subq_indx, parse->rtable));
 
 	return subq_indx;
 }
@@ -843,7 +843,7 @@ add_notin_subquery_rte(Query *parse, Query *subselect)
 static int
 add_expr_subquery_rte(Query *parse, Query *subselect)
 {
-	RangeTblEntry *subq_rte;
+	ParseNamespaceItem *subq_nsitem;
 	int			subq_indx;
 
 	/**
@@ -866,16 +866,16 @@ add_expr_subquery_rte(Query *parse, Query *subselect)
 	 * It is marked as lateral, because any correlation quals will
 	 * refer to other RTEs in the parent query.
 	 */
-	subq_rte = addRangeTableEntryForSubquery(NULL,	/* pstate */
+	subq_nsitem = addRangeTableEntryForSubquery(NULL,	/* pstate */
 											 subselect,
 											 makeAlias("Expr_SUBQUERY", NIL),
 											 true, /* lateral */
 											 false /* inFromClause */ );
-	parse->rtable = lappend(parse->rtable, subq_rte);
+	parse->rtable = lappend(parse->rtable, subq_nsitem->p_rte);
 
 	/* assume new rte is at end */
 	subq_indx = list_length(parse->rtable);
-	Assert(subq_rte == rt_fetch(subq_indx, parse->rtable));
+	Assert(subq_nsitem->p_rte == rt_fetch(subq_indx, parse->rtable));
 
 	return subq_indx;
 }
