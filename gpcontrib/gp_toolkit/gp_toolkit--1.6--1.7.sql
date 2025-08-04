43903fd7d092de3efaa0cb25f2e9b9c0d03afd01
Alexander Kondakov <a.kondakov@arenadata.io>
2025-08-05 00:09:42 +0300

    minor fix

--- gpcontrib/gp_toolkit/gp_toolkit--1.6--1.7.sql
+++ gpcontrib/gp_toolkit/gp_toolkit--1.6--1.7.sql
@@ -5,17 +5,17 @@
 
 -- This function set the numsegments when creating tables during rebalance operation.
 -- This form accepts an integer argument: [1, gp_num_contents_in_cluster].
-CREATE OR REPLACE FUNCTION gp_toolkit.gp_set_rebalance_numsegments(integer) RETURNS text
-AS 'gp_toolkit.so','gp_set_rebalance_numsegments'
+CREATE FUNCTION gp_toolkit.gp_set_rebalance_numsegments(integer) RETURNS text
+AS '$libdir/gp_toolkit','gp_set_rebalance_numsegments'
 LANGUAGE C STRICT;
 
 -- This function reset the default numsegments.
 -- This function resets numsegments directly to default value INT_MAX
-CREATE OR REPLACE FUNCTION gp_toolkit.gp_reset_rebalance_numsegments() RETURNS void
-AS 'gp_toolkit.so','gp_reset_rebalance_numsegments'
+CREATE FUNCTION gp_toolkit.gp_reset_rebalance_numsegments() RETURNS void
+AS '$libdir/gp_toolkit','gp_reset_rebalance_numsegments'
 LANGUAGE C STRICT;
 
 -- This function get the default numsegments when creating tables.
-CREATE OR REPLACE FUNCTION gp_toolkit.gp_get_rebalance_numsegments() RETURNS text
-AS 'gp_toolkit.so','gp_get_rebalance_numsegments'
+CREATE FUNCTION gp_toolkit.gp_get_rebalance_numsegments() RETURNS text
+AS '$libdir/gp_toolkit','gp_get_rebalance_numsegments'
 LANGUAGE C STRICT;
