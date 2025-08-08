c4ebcffd1545e5071860e1f530252be0749cb29e
Alexander Kondakov <a.kondakov@arenadata.io>
2025-08-08 13:01:50 +0300

    Use shared variable for default number of segments (#1941)
    Commit 5b3f506b7bb0be61e8bec6c32e7b16cd764fc652 introduced new command ALTER
    TABLE REBALANCE with shrink support. The target number of segments (if not
    specified in ALTER command) is taken from GP_POLICY_DEFAULT_NUMSEGMENTS() macro.
    Therefore, we need somehow to set and maintain the creation number across all
    backends.
    
    This patch introduces a mechanism for managing the default number of segments
    used in table creation during a rebalance operation in GPDB. A new shared
    variable gp_create_table_rebalance_numsegments is introduced in gpexpand.h  to
    track the number of segments to use during table creation while a rebalancing
    operation is in progress. The shared variable is initialized in shared memory
    with appropriate size and get functionality.
    
    Corresponding SQL functions are created in gp_toolkit extension.
    The system now checks if a rebalancing operation is active by verifying locks
    before allowing modifications to the number of segments. If a lock is not
    already acquired in current transaction (indicating that no rebalancing is
    underway), an appropriate error message is returned.
    
    Tests from 5b3f506b7bb0be61e8bec6c32e7b16cd764fc652
    are updated to support the new functonality
    
    gp_debug_numsegments extension preserves its behaviour. But we disallow to
    modify local numsegments value when gp_create_table_rebalance_numsegments
    is set.

--- src/backend/storage/ipc/ipci.c
+++ src/backend/storage/ipc/ipci.c
@@ -227,6 +227,9 @@ CreateSharedMemoryAndSemaphores(int port)
 		/* size of expand version */
 		size = add_size(size, GpExpandVersionShmemSize());
 
+		/* size of rebalance numsegments variable */
+		size = add_size(size, GpRebalanceNumsegmentsShmemSize());
+
 		/* size of token and endpoint shared memory */
 		size = add_size(size, EndpointShmemSize());
 
@@ -384,6 +387,8 @@ CreateSharedMemoryAndSemaphores(int port)
 		InstrShmemInit();
 
 	GpExpandVersionShmemInit();
+
+	GpRebalanceNumsegmentsShmemInit();
 
 #ifdef EXEC_BACKEND
 
