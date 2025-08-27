dca79e5f8cb088b00c0becd09ed844c6d079b889
Georgy Shelkovy <g.shelkovy@arenadata.io>
2025-08-27 09:23:10 +0500

    Fix compilation of setrefs.c, nodeDynamicSeqscan.c, nodeDynamicIndexscan.c, nodeDynamicBitmapHeapscan.c, transform.c and cdbsubselect.c (#1920)
    1) Commit 9ce77d7 renamed the varnoold and varoattno fields in the Var
    structure to varnosyn and varattnosyn, we need to do the same in GPDB-specific
    places.
    
    2) Commit e1551f9 renamed convert_tuples_by_name_map_if_req to
    build_attrmap_by_name_if_req and changed return type from AttrNumber to
    AttrMap, we need to do the same in GPDB-specific places.
    
    3) Commit 5815696 changed the return type of the addRangeTableEntryForSubquery
    function from RangeTblEntry to ParseNamespaceItem, we also need to do this in
    the GPDB-specific code.

--- src/backend/optimizer/plan/setrefs.c
+++ src/backend/optimizer/plan/setrefs.c
@@ -226,8 +226,8 @@ static void set_plan_references_input_asserts(PlannerInfo *root, Plan *plan)
 
 #if 0
 		/* ModifyTable plans have a funny target list, set up just for EXPLAIN. */
-		if (!IsA(plan, ModifyTable) && var->varno != var->varnoold)
-			Assert(false && "Varno and varnoold do not agree!");
+		if (!IsA(plan, ModifyTable) && var->varno != var->varnosyn)
+			Assert(false && "Varno and varnosyn do not agree!");
 #endif
 	}
 }
@@ -2555,15 +2555,16 @@ set_splitupdate_tlist_references(Plan *plan, int rtoffset)
 						 exprTypmod((Node *) oldvar),
 						 exprCollation((Node *) oldvar),
 						 0);
-		if (IsA(oldvar, Var))
+		if (IsA(oldvar, Var) &&
+			oldvar->varnosyn > 0)
 		{
-			newvar->varnoold = oldvar->varno + rtoffset;
-			newvar->varoattno = oldvar->varattno;
+			newvar->varnosyn = oldvar->varnosyn + rtoffset;
+			newvar->varattnosyn = oldvar->varattnosyn;
 		}
 		else
 		{
-			newvar->varnoold = 0;		/* wasn't ever a plain Var */
-			newvar->varoattno = 0;
+			newvar->varnosyn = 0;		/* wasn't ever a plain Var */
+			newvar->varattnosyn = 0;
 		}
 
 		tle = flatCopyTargetEntry(tle);
