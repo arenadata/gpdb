019b839175a7f06bf7582c2e1430e6e2d568c454
Georgy Shelkovy <g.shelkovy@arenadata.io>
2025-11-06 09:36:13 +0500

    Resolve conflicts in vacuum.h, vacuum.c, and vacuumlazy.c
    1) Commit 40d964e added a new field, nworkers, to the VacuumParams structure in
    src/include/commands/vacuum.h, while earlier commit 781d7fe had already added
    another new field, auto_stats, to the same location.
    
    2) Commit 40d964e added a declaration of a new function,
    compute_parallel_delay, to src/backend/commands/vacuum.c, while earlier commit
    5778f31 had already added a GPDB-specific argument, recursing, to the
    declaration of the previous function, vacuum_rel.
    
    3) Commit 40d964e added a new local variable, parallel_option, to the
    ExecVacuum function in src/backend/commands/vacuum.c, while the earlier commit
    19cd1cf had already added the new GPDB-specific local variables, rootonly,
    fullscan, and ao_phase, to the same location.
    
    4) Commit 40d964e added handling of the new parallel option to the ExecVacuum
    function in src/backend/commands/vacuum.c, while the earlier commit 19cd1cf had
    already added handling of the GPDB-specific ao_phase option to the same
    location.
    
    5) Commit 40d964e added a new include access/parallel.h to
    src/backend/access/heap/vacuumlazy.c, while earlier commit 17c4dd1 had already
    added another include access/nbtree.h to the same location.
    
    6) Commit 40d964e replaced the conditional update of vac_update_relstats
    statistics in the lazy_cleanup_index function in
    src/backend/access/heap/vacuumlazy.c with conditional messages, while earlier
    commit e5d1779 had already added the final argument, isvacuum, to the
    vac_update_relstats function call.

--- src/include/commands/vacuum.h
+++ src/include/commands/vacuum.h
@@ -283,9 +283,7 @@ typedef struct VacuumParams
 										 * default value depends on reloptions */
 	VacOptTernaryValue truncate;	/* Truncate empty pages at the end,
 									 * default value depends on reloptions */
-<<<<<<< HEAD
 	bool auto_stats;      /* invoked via automatic statistic collection */
-=======
 
 	/*
 	 * The number of parallel vacuum workers.  0 by default which means choose
@@ -293,7 +291,6 @@ typedef struct VacuumParams
 	 * disabled.
 	 */
 	int			nworkers;
->>>>>>> a91e2fa94180f24dd68fb6c99136cda820e02089
 } VacuumParams;
 
 typedef struct
