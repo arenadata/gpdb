76f307ae4c4d4e18af62aee846aeb80ac433a632
Denis Kovalev <d.kovalev@arenadata.io>
2025-11-07 08:33:51 +0300

    Fix conflicts related to pg_stat_get_activity (#1989)
    Commit b025f32 added leader_pid to the pg_stat_activity. GPDB has its own
    additions to it, which made a conflicts to src/backend/catalog/system_views.sql,
    src/include/catalog/pg_proc.dat and src/backend/utils/adt/pgstatfuncs.c.
    
    This commit resolves conflicts in a way that Postgres changes are clustered,
    allowing easier conflict resolution in a future.

--- src/backend/utils/adt/pgstatfuncs.c
+++ src/backend/utils/adt/pgstatfuncs.c
@@ -553,11 +553,7 @@ pg_stat_get_progress_info(PG_FUNCTION_ARGS)
 Datum
 pg_stat_get_activity(PG_FUNCTION_ARGS)
 {
-<<<<<<< HEAD
-#define PG_STAT_GET_ACTIVITY_COLS	32
-=======
-#define PG_STAT_GET_ACTIVITY_COLS	30
->>>>>>> a91e2fa94180f24dd68fb6c99136cda820e02089
+#define PG_STAT_GET_ACTIVITY_COLS	33
 	int			num_backends = pgstat_fetch_stat_numbackends();
 	int			curr_backend;
 	int			pid = PG_ARGISNULL(0) ? -1 : PG_GETARG_INT32(0);
@@ -700,8 +696,10 @@ pg_stat_get_activity(PG_FUNCTION_ARGS)
 			nulls[29] = true;
 
 			proc = BackendPidGetProc(beentry->st_procpid);
+			if (proc != NULL)
+			{
+				uint32		raw_wait_event;
 
-<<<<<<< HEAD
 				raw_wait_event = UINT32_ACCESS_ONCE(proc->wait_event_info);
 				wait_event_type = pgstat_get_wait_event_type(raw_wait_event);
 
@@ -722,15 +720,23 @@ pg_stat_get_activity(PG_FUNCTION_ARGS)
 				}
 			}
 			else if (beentry->st_backendType != B_BACKEND)
-=======
-			if (proc == NULL && (beentry->st_backendType != B_BACKEND))
->>>>>>> a91e2fa94180f24dd68fb6c99136cda820e02089
 			{
 				/*
 				 * For an auxiliary process, retrieve process info from
 				 * AuxiliaryProcs stored in shared-memory.
 				 */
 				proc = AuxiliaryPidGetProc(beentry->st_procpid);
+
+				if (proc != NULL)
+				{
+					uint32		raw_wait_event;
+
+					raw_wait_event =
+						UINT32_ACCESS_ONCE(proc->wait_event_info);
+					wait_event_type =
+						pgstat_get_wait_event_type(raw_wait_event);
+					wait_event = pgstat_get_wait_event(raw_wait_event);
+				}
 			}
 
 			/*
@@ -741,12 +747,7 @@ pg_stat_get_activity(PG_FUNCTION_ARGS)
 			 */
 			if (proc != NULL)
 			{
-				uint32		raw_wait_event;
 				PGPROC	   *leader;
-
-				raw_wait_event = UINT32_ACCESS_ONCE(proc->wait_event_info);
-				wait_event_type = pgstat_get_wait_event_type(raw_wait_event);
-				wait_event = pgstat_get_wait_event(raw_wait_event);
 
 				leader = proc->lockGroupLeader;
 				if (leader)
@@ -922,17 +923,17 @@ pg_stat_get_activity(PG_FUNCTION_ARGS)
 													 * use */
 			}
 
-			values[29] = Int32GetDatum(beentry->st_session_id);  /* GPDB */
+			values[30] = Int32GetDatum(beentry->st_session_id);  /* GPDB */
 
 			{
 				char *groupName = GetResGroupNameForId(beentry->st_rsgid);
 
-				values[30] = ObjectIdGetDatum(beentry->st_rsgid);
+				values[31] = ObjectIdGetDatum(beentry->st_rsgid);
 
 				if (groupName != NULL)
-					values[31] = CStringGetTextDatum(groupName);
+					values[32] = CStringGetTextDatum(groupName);
 				else
-					nulls[31] = true;
+					nulls[32] = true;
 			}
 		}
 		else
@@ -961,14 +962,10 @@ pg_stat_get_activity(PG_FUNCTION_ARGS)
 			nulls[26] = true;
 			nulls[27] = true;
 			nulls[28] = true;
-<<<<<<< HEAD
-
-			values[29] = Int32GetDatum(beentry->st_session_id);
-			nulls[30] = true;
-			nulls[31] = true;
-=======
 			nulls[29] = true;
->>>>>>> a91e2fa94180f24dd68fb6c99136cda820e02089
+			values[30] = Int32GetDatum(beentry->st_session_id);
+			nulls[31] = true;
+			nulls[32] = true;
 		}
 
 		tuplestore_putvalues(tupstore, tupdesc, values, nulls);
