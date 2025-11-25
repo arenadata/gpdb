7d0f76e6923fd73b60410c8d9d05902c3e8ba2ad
Denis Kovalev <d.kovalev@arenadata.io>
2025-11-25 14:23:49 +0300

    Fix conflict in src/bin/pg_basebackup/pg_basebackup.c

--- src/bin/pg_basebackup/pg_basebackup.c
+++ src/bin/pg_basebackup/pg_basebackup.c
@@ -409,15 +409,11 @@ usage(void)
 	printf(_("  -U, --username=NAME    connect as specified database user\n"));
 	printf(_("  -w, --no-password      never prompt for password\n"));
 	printf(_("  -W, --password         force password prompt (should happen automatically)\n"));
-<<<<<<< HEAD
 	printf(_("  -E, --exclude          exclude path names\n"));
 	printf(_("      --exclude-from=FILE\n"
 			 "                         get path names to exclude from FILE\n"));
-	printf(_("\nReport bugs to <bugs@greenplum.org>.\n"));
-=======
 	printf(_("\nReport bugs to <%s>.\n"), PACKAGE_BUGREPORT);
 	printf(_("%s home page: <%s>\n"), PACKAGE_NAME, PACKAGE_URL);
->>>>>>> 4dbcb3f844eca4a401ce06aa2781bd9a9be433e9
 }
 
 
