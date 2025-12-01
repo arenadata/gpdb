fe6d0debd49a4fe4c350b00b0dc0e2cde73bb571
Denis Garsh <d.garsh@arenadata.io>
2025-12-01 08:45:34 +0300

    Fix tests in src/test/regress/sql/strings.sql

--- src/test/regress/sql/strings.sql
+++ src/test/regress/sql/strings.sql
@@ -669,16 +669,6 @@ set standard_conforming_strings = off;
 select 'a\\bcd' as f1, 'a\\b\'cd' as f2, 'a\\b\'''cd' as f3, 'abcd\\'   as f4, 'ab\\\'cd' as f5, '\\\\' as f6;
 
 --
--- test unicode escape
---
-select E'A\u0041' as f1, E'\u0127' as f2;
-select E'\u0000';
-select E'\udsfs';
-select E'\uD843\uE001';
-select E'\uDC01';
-select E'\uD834';
-
---
 -- Additional string functions
 --
 SET bytea_output TO escape;
