035fdc1eed9fc6040dbb39865cb20e0f7861375f
Maksim Michkov <m.michkov@arenadata.io>
2026-04-10 16:47:18 +0300

    Fix test src/test/regress/expected/gin.out
    Commit e41955faf060f90918303ce0623df9d765144bf6 added new tests whose
    output should be adapted for GPDB, including a separate output for ORCA.
    Also, move GPDB-specific set enable_seqscan = off; to before the new tests
    to make the planner choose to use indexes instead.

--- src/test/regress/sql/gin.sql
+++ src/test/regress/sql/gin.sql
@@ -36,6 +36,8 @@ insert into gin_test_tbl select array[1, 3, g] from generate_series(1, 1000) g;
 delete from gin_test_tbl where i @> array[2];
 vacuum gin_test_tbl;
 
+set enable_seqscan = off;
+
 -- Test for "rare && frequent" searches
 explain (costs off)
 select count(*) from gin_test_tbl where i @> array[1, 999];
@@ -73,7 +75,6 @@ values
   ('{1,3}', '{}'),
   ('{1,1}', '{10}');
 
-set enable_seqscan = off;
 explain (costs off)
 select * from t_gin_test_tbl where array[0] <@ i;
 select * from t_gin_test_tbl where array[0] <@ i;
