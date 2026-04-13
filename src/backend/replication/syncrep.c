7b929d0452c7870b0b4c4d8785a33717766b7c6b
Georgy Shelkovy <g.shelkovy@arenadata.io>
2026-04-13 09:04:45 +0500

    fix

--- src/backend/replication/syncrep.c
+++ src/backend/replication/syncrep.c
@@ -898,6 +898,9 @@ SyncRepGetCandidateStandbys(SyncRepStandbyData **standbys)
 		n++;
 	}
 
+	if (IS_QUERY_DISPATCHER())
+		return n;
+
 	/*
 	 * In quorum mode, we return all the candidates.  In priority mode, if we
 	 * have too many candidates then return only the num_sync ones of highest
