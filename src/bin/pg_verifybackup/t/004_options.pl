7767af5b1bdbfc38c5fb228162c07e117db55ae3
Georgy Shelkovy <g.shelkovy@arenadata.io>
2026-04-14 11:44:01 +0500

    Fix tests in src/bin/pg_verifybackup/t/*.pl
    Commit 0d8c9c1 added new tests to src/bin/pg_validatebackup/t/*.pl, and commit
    dbc60c5 renamed pg_validatebackup to pg_verifybackup. In the GPDB, we need to
    add the target-gp-dbid option when backuping. PostgresNode.pm wrote comments to
    the internal.auto.conf file, but pg_basebackup did not, causing pg_verifybackup
    to see file size differences. Don't write comments.

--- src/bin/pg_verifybackup/t/004_options.pl
+++ src/bin/pg_verifybackup/t/004_options.pl
@@ -14,7 +14,8 @@ my $master = get_new_node('master');
 $master->init(allows_streaming => 1);
 $master->start;
 my $backup_path = $master->backup_dir . '/test_options';
-$master->command_ok(['pg_basebackup', '-D', $backup_path, '--no-sync' ],
+$master->command_ok(['pg_basebackup', '-D', $backup_path, '--no-sync',
+					'--target-gp-dbid', '1'],
 					"base backup ok");
 
 # Verify that pg_verifybackup -q succeeds and produces no output.
--- src/bin/pg_verifybackup/t/007_wal.pl
+++ src/bin/pg_verifybackup/t/007_wal.pl
@@ -14,7 +14,8 @@ my $master = get_new_node('master');
 $master->init(allows_streaming => 1);
 $master->start;
 my $backup_path = $master->backup_dir . '/test_wal';
-$master->command_ok(['pg_basebackup', '-D', $backup_path, '--no-sync' ],
+$master->command_ok(['pg_basebackup', '-D', $backup_path, '--no-sync',
+					'--target-gp-dbid', '1'],
 					"base backup ok");
 
 # Rename pg_wal.
