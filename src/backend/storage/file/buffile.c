9ab665344a145555bd137a651101e51a8997e00e
Georgy Shelkovy <g.shelkovy@arenadata.io>
2026-04-17 21:01:02 +0500

    fix

--- src/backend/storage/file/buffile.c
+++ src/backend/storage/file/buffile.c
@@ -775,7 +775,7 @@ BufFileWrite(BufFile *file, void *ptr, size_t size)
 
 		case BFS_COMPRESSED_WRITING:
 			BufFileDumpCompressedBuffer(file, ptr, size);
-			return size;
+			return;
 
 		case BFS_SEQUENTIAL_READING:
 		case BFS_COMPRESSED_READING:
