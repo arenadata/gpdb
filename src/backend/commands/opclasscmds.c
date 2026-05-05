079c76962f8ca1fff817699f5c36c2d67c6d885d
Maksim Michkov <m.michkov@arenadata.io>
2026-05-05 14:58:38 +0300

    Resolve conflicts in src/backend/commands/opclasscmds.c
    Commit 9f9682783bea74bf8d93cac4f7dd65fa677f5dc7 removed header
    opfam_internal.h, however there was GPDB-specific include nearby.

--- src/backend/commands/opclasscmds.c
+++ src/backend/commands/opclasscmds.c
@@ -27,11 +27,7 @@
 #include "catalog/dependency.h"
 #include "catalog/indexing.h"
 #include "catalog/objectaccess.h"
-<<<<<<< HEAD
 #include "catalog/oid_dispatch.h"
-#include "catalog/opfam_internal.h"
-=======
->>>>>>> d259afa7365165760004c2fdbe2520a94ddf2600
 #include "catalog/pg_am.h"
 #include "catalog/pg_amop.h"
 #include "catalog/pg_amproc.h"
