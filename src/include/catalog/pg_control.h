15c0a84ec687c145153a68d68d69018883ca5a04
Viktor Kurilko <v.kurilko@arenadata.io>
2026-05-08 12:22:00 +0700

    Resolve conflicts in the pg_controldata.c, pg_resetwal.c and pg_control.h (#2444)
    Commit fea10a64340e529805609126740a540c8f9daab4 renamed a field in the
    CheckPoint, but commit 24e274093461727b305fda4bae8ea41e2de6507b added other
    field nearby.
    
    In pg_controldata.c commit 24e274093461727b305fda4bae8ea41e2de6507b added printf
    nearby.
    In pg_resetwal.c commits 26bc6228baa852caf458fb3c36ff4f03a8a8b6bd and
    04d26d7d9e78a309a005adc62be03fb76c89c04a added code nearby.

--- src/include/catalog/pg_control.h
+++ src/include/catalog/pg_control.h
@@ -45,12 +45,8 @@ typedef struct CheckPoint
 	TimeLineID	PrevTimeLineID; /* previous TLI, if this record begins a new
 								 * timeline (equals ThisTimeLineID otherwise) */
 	bool		fullPageWrites; /* current full_page_writes */
-<<<<<<< HEAD
-	FullTransactionId nextFullXid;	/* next free full transaction ID */
-	DistributedTransactionId nextGxid;	/* next free gxid */
-=======
 	FullTransactionId nextXid;	/* next free transaction ID */
->>>>>>> d259afa7365165760004c2fdbe2520a94ddf2600
+	DistributedTransactionId nextGxid;	/* next free gxid */
 	Oid			nextOid;		/* next free OID */
 	Oid			nextRelfilenode;	/* next free Relfilenode */
 	MultiXactId nextMulti;		/* next free MultiXactId */
