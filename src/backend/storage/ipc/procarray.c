50fac289292f507e217575a9a98026c1164e0f23
Georgy Shelkovy <g.shelkovy@arenadata.io>
2026-05-13 09:51:45 +0500

    rm

--- src/backend/storage/ipc/procarray.c
+++ src/backend/storage/ipc/procarray.c
@@ -4800,7 +4800,6 @@ GlobalVisTestFor(Relation rel)
 		Assert(rel->rd_rel->relkind == RELKIND_RELATION ||
 			   rel->rd_rel->relkind == RELKIND_MATVIEW ||
 			   rel->rd_rel->relkind == RELKIND_TOASTVALUE ||
-			   rel->rd_rel->relkind == RELKIND_PARTITIONED_TABLE ||
 			   rel->rd_rel->relkind == RELKIND_AOSEGMENTS ||
 			   rel->rd_rel->relkind == RELKIND_AOBLOCKDIR ||
 			   rel->rd_rel->relkind == RELKIND_AOVISIMAP);
