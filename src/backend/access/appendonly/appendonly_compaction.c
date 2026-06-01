2e12c350997a9e91d724b9592660c6b4e4417021
Georgy Shelkovy <g.shelkovy@arenadata.io>
2026-06-01 14:16:13 +0500

    Fix compilation of aocs_compaction.c and appendonly_compaction.c (#2620)
    1) Commit a04daa9 in src/include/executor/executor.h added a new
    argument, ResultRelInfo *resultRelInfo, to the definition of the
    ExecInsertIndexTuples function. Add this in GPDB-specific places.
    
    2) Commit 1375422 in src/include/nodes/execnodes.h changed the type of
    the es_result_relations field in the EState structure from
    ResultRelInfo * to ResultRelInfo **. Change this in GPDB-specific
    places.

--- src/backend/access/appendonly/appendonly_compaction.c
+++ src/backend/access/appendonly/appendonly_compaction.c
@@ -301,7 +301,8 @@ AppendOnlyMoveTuple(TupleTableSlot *slot,
 	/* insert index' tuples if needed */
 	if (resultRelInfo->ri_NumIndices > 0)
 	{
-		ExecInsertIndexTuples(slot,
+		ExecInsertIndexTuples(resultRelInfo,
+							  slot,
 							  estate,
 							  false, /* noDupError */
 							  NULL, /* specConflict */
@@ -444,7 +445,10 @@ AppendOnlySegmentFileFullCompaction(Relation aorel,
 	resultRelInfo->ri_RelationDesc = aorel;
 	resultRelInfo->ri_TrigDesc = NULL;	/* we don't fire triggers */
 	ExecOpenIndices(resultRelInfo, false);
-	estate->es_result_relations = resultRelInfo;
+	if (estate->es_result_relations == NULL)
+		estate->es_result_relations = (ResultRelInfo **)
+			palloc0(estate->es_range_table_size * sizeof(ResultRelInfo *));
+	estate->es_result_relations[resultRelInfo->ri_RangeTableIndex - 1] = resultRelInfo;
 
 	/*
 	 * We don't want uniqueness checks to be performed while "insert"ing tuples
