1536691dcf50567c9a4d962ad9cc017c4400e6ef
Alexander Kondakov <a.kondakov@arenadata.io>
2026-06-05 13:19:20 +0300

    Fix conflicts in create_table_like.sql (#2641)
    Commit 19f5a37 added the new test in create_table_like.sql, while
    upstream
    has modified the same area in 598f4b0. Resolve the conflict and fix the
    output.

--- src/test/regress/sql/create_table_like.sql
+++ src/test/regress/sql/create_table_like.sql
@@ -165,16 +165,12 @@ SELECT s.stxname, objsubid, description FROM pg_description, pg_statistic_ext s 
 CREATE TABLE inh_error1 () INHERITS (ctlt1, ctlt4);
 CREATE TABLE inh_error2 (LIKE ctlt4 INCLUDING STORAGE) INHERITS (ctlt1);
 
-<<<<<<< HEAD
-DROP TABLE ctlt12_storage, ctlt12_comments, ctlt1_inh, ctlt13_inh, ctlt13_like, ctlt_all, ctlb, ctla, ctlt1, ctlt2, ctlt3, ctlt4 CASCADE;
-=======
 -- Check that LIKE isn't confused by a system catalog of the same name
 CREATE TABLE pg_attrdef (LIKE ctlt1 INCLUDING ALL);
 \d+ public.pg_attrdef
 DROP TABLE public.pg_attrdef;
 
-DROP TABLE ctlt1, ctlt2, ctlt3, ctlt4, ctlt12_storage, ctlt12_comments, ctlt1_inh, ctlt13_inh, ctlt13_like, ctlt_all, ctla, ctlb CASCADE;
->>>>>>> f81e97d0475cd4bc597adc23b665bd84fbf79a0d
+DROP TABLE ctlt12_storage, ctlt12_comments, ctlt1_inh, ctlt13_inh, ctlt13_like, ctlt_all, ctlb, ctla, ctlt1, ctlt2, ctlt3, ctlt4 CASCADE;
 
 -- LIKE must respect NO INHERIT property of constraints
 CREATE TABLE noinh_con_copy (a int CHECK (a > 0) NO INHERIT);
